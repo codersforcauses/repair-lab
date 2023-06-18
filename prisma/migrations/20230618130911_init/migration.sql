c8c14655e3274c3d0dd0ae687af60e9643f54df3
Dylan To <dylan.to@hotmail.com>
2023-06-18 21:10:21 +0800

    Fix migrations

--- prisma/migrations/20230618130911_init/migration.sql
+++ prisma/migrations/20230618130911_init/migration.sql
@@ -2,10 +2,9 @@
 CREATE TABLE "Message" (
     "id" SERIAL NOT NULL,
     "content" TEXT NOT NULL,
-    "authorId" INTEGER NOT NULL,
+    "authorId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "User" TEXT NOT NULL,
 
     CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
