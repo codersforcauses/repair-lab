6885c8ef9fb4c33c2ce02467b1443fe62f7f2bd9
Vyvienne Bautista <vbf.bautista@gmail.com>
2023-06-27 20:27:50 +0800

    Created first event - Bob The Builder

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+
+  const eventNonDefaultFilled = await prisma.event.upsert({
+    where: { name: 'Can Bob Fix It?' },
+    update: { name: 'Can Bob Fix It? [UPDATED]'},
+    create: {
+      createdBy: 'Bob (The) Builder',
+
+      name: 'Can Bob Fix It?',
+      location: 'Bobsville',
+      description: 'A general contractor from Bobsville provides his services in Bobsville.',
+      volunteers: [],
+
+      startDate: new Date(2023, 5, 28, 15, 30, 0, 0),
+      endDate: new Date(2023, 6, 1, 15, 30, 0, 0),
+      }
+    })
+  
+  console.log({ eventNonDefaultFilled })
+}
