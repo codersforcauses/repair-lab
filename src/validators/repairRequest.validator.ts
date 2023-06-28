97d3fa0c72ccd20b89718b6f985263e0849c335e
Justin Liang <justin4426@outlook.com>
2023-06-28 22:18:05 +0800

    chore: add min letters to description field

--- src/validators/repairRequest.validator.ts
+++ src/validators/repairRequest.validator.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const postSchema = z.object({
   eventId: z.string().uuid(),
-  description: z.string().nonempty(),
+  description: z.string().nonempty().min(5), // ? Have a min number of words for description?
   itemType: z.string().nonempty(),
   itemBrand: z.string().nonempty()
 });
