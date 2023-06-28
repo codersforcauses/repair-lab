ba48b2f64a437e67555b1468b56d9f4a4a3b3603
wintelestr <133135894+wintelestr@users.noreply.github.com>
2023-06-28 19:31:31 +0800

    Add itemType.ts

---
+++ src/pages/api/itemType.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      getItemTypes(req, res);
+      break;
+    default:
+      return res.status(405).json({
+        error: { message: `Method ${req.method} not allowed` }
+      });
+  }
+}
+
+const getItemTypes = async (req: NextApiRequest, res: NextApiResponse) => {
+  const itemtypes = await prisma.itemType.findMany();
+
+  return res.status(200).json(itemtypes);
+};
+
+export const config = {};
