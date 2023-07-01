beffa2b6f6b4ab80d1eb8935d8d9a8e6d5fdb801
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-07-01 12:09:29 +0800

    first draft layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,67 @@
+import React, { useState } from 'react';
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform login logic here with username and password
+    console.log('Logging in...');
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Reset the form
+    setUsername('');
+    setPassword('');
+  };
+
   return (
-    <main>
-      <h1>Login Page</h1>
+    <main className="flex justify-center items-center h-screen">
+      <div className="w-64 bg-white rounded shadow p-8">
+        <h1 className="text-2xl font-bold mb-4">Login Page</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block mb-2 font-medium">
+              Username:
+            </label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+              placeholder="Enter your username"
+              className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block mb-2 font-medium">
+              Password:
+            </label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Enter your password"
+              className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
