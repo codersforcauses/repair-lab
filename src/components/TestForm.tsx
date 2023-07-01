533c53241eb0448ea562c5b1cd2fb1a92c8b51d9
Stanley Natalius <22915411@student.uwa.edu.au>
2023-07-01 12:25:57 +0800

    Added text area as optional input field

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -20,9 +20,9 @@ const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <TextInput textArea label="Other" {...register("other")}/>
         <TextInput label="Email" placeholder="Enter Email" icon="https://file.rendit.io/n/WO0yqXIkWlVzApILek8q.svg" required {...register("email")}/>
         <TextInput label="Name" placeholder="Enter Name" {...register("name")}/>
-        <TextInput label="Other" placeholder="What is this" {...register("other")}/>
         <button type="submit">Submit</button>
       </form>
     </div>
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,8 +1,33 @@
 import Image from "next/image";
 
-const TextInput = ({label, required = false, placeholder, icon=""}) => {
+const TextInput = ({textArea = false, label, required = false, placeholder = "", icon=""}) => {
+  if (placeholder == ""){
+    placeholder = "Enter " + label;
+  }
   return(
-  <div className="relative flex h-12 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow">
+    textArea ? (
+      <div className="relative flex h-24 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow mb-3">
+      <div className="absolute -top-2 left-2 flex flex-row items-center gap-0.5 rounded-full bg-white px-1">
+        <label className="text-xs font-semibold text-black">{label}</label>
+        {required ? <span className="text-xs font-semibold text-[#FF0000]">*</span> : ''}
+      </div>
+      <textarea
+        className="mr-1 w-full text-sm placeholder:text-gray-500 focus:outline-none focus:ring-0"
+        placeholder={placeholder}
+        id = {label}
+      />
+      {icon != "" ? (
+        <Image
+          src={icon}
+          alt="icon"
+          width={16}
+          height={16}
+          className="relative min-h-0 w-4 min-w-0 shrink-0"
+        />
+      ) : ""}
+      </div>
+    ) : (
+      <div className="relative flex h-12 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow mb-2">
       <div className="absolute -top-2 left-2 flex flex-row items-center gap-0.5 rounded-full bg-white px-1">
         <label className="text-xs font-semibold text-black">{label}</label>
         {required ? <span className="text-xs font-semibold text-[#FF0000]">*</span> : ''}
@@ -22,6 +47,8 @@ const TextInput = ({label, required = false, placeholder, icon=""}) => {
         />
       ) : ""}
       </div>
+    )
+  
   )
 }
 
