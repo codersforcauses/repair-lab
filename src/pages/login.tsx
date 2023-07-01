2e42ae7883c97fe1d384093f9692e8e62e5c3383
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-07-01 14:40:03 +0800

    draft-layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import CustomButton from "../components/CustomButton";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -14,19 +15,15 @@ export default function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Perform login logic here with username and password
-    console.log('Logging in...');
-    console.log('Username:', username);
-    console.log('Password:', password);
     // Reset the form
-    setUsername('');
-    setPassword('');
+    setUsername("");
+    setPassword("");
   };
 
   return (
-    <main className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen">
       <div className="w-64 bg-white rounded shadow p-8">
-        <h1 className="text-2xl font-bold mb-4">Login Page</h1>
+        <h1 className="text-2xl font-bold mb-4">Sign in</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="username" className="block mb-2 font-medium">
@@ -37,8 +34,8 @@ export default function Login() {
               type="text"
               value={username}
               onChange={handleUsernameChange}
-              placeholder="Enter your username"
-              className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-blue-500"
+              placeholder="Username"
+              className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500 input-field"
             />
           </div>
           <div className="mb-4">
@@ -50,18 +47,25 @@ export default function Login() {
               type="password"
               value={password}
               onChange={handlePasswordChange}
-              placeholder="Enter your password"
-              className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-blue-500"
+              placeholder="Password"
+              className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500 input-field"
             />
           </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-          >
-            Login
-          </button>
+          <div className="mt-4 text-center">
+            <a href="/forgot-password" className="text-coolGray-50 text-xs">
+              Forgot your password?
+            </a>
+          </div>
+          <div className="mt-2 text-center">
+            <a href="/signup" className="text-coolGray-50 text-xs">
+              Don't have an account? Sign up
+            </a>
+          </div>
+          <div className="w-full">
+            <CustomButton onClick={handleSubmit}>Sign in</CustomButton>
+          </div>
         </form>
       </div>
-    </main>
+    </div>
   );
 }
