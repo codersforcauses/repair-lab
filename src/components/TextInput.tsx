2d991e215d34d7847863a65bd3ac6ce10890bf5f
Stanley Natalius <22915411@student.uwa.edu.au>
2023-07-01 14:19:43 +0800

    Added formContext, now works w/ react hook form

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
+import { useFormContext } from "react-hook-form";
 
-const TextInput = ({textArea = false, label, required = false, placeholder = "", icon=""}) => {
-  if (placeholder == ""){
+const TextInput = ({
+  textArea = false,
+  label = "",
+  required = false,
+  placeholder = "",
+  icon = "",
+  name = ""
+}) => {
+  const { register } = useFormContext();
+
+  if (placeholder == "") {
     placeholder = "Enter " + label;
   }
-  return(
-    textArea ? (
-      <div className="relative flex h-24 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow mb-3">
+  return textArea ? (
+    <div className="relative mb-3 flex h-24 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow">
       <div className="absolute -top-2 left-2 flex flex-row items-center gap-0.5 rounded-full bg-white px-1">
         <label className="text-xs font-semibold text-black">{label}</label>
-        {required ? <span className="text-xs font-semibold text-[#FF0000]">*</span> : ''}
+        {required ? (
+          <span className="text-xs font-semibold text-[#FF0000]">*</span>
+        ) : (
+          ""
+        )}
       </div>
       <textarea
         className="mr-1 w-full text-sm placeholder:text-gray-500 focus:outline-none focus:ring-0"
         placeholder={placeholder}
-        id = {label}
+        id={label}
+        {...register(name)}
       />
       {icon != "" ? (
         <Image
@@ -24,18 +38,25 @@ const TextInput = ({textArea = false, label, required = false, placeholder = "",
           height={16}
           className="relative min-h-0 w-4 min-w-0 shrink-0"
         />
-      ) : ""}
-      </div>
-    ) : (
-      <div className="relative flex h-12 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow mb-2">
+      ) : (
+        ""
+      )}
+    </div>
+  ) : (
+    <div className="relative mb-2 flex h-12 w-64 flex-row items-center justify-between rounded-lg border border-gray-300 px-3 shadow">
       <div className="absolute -top-2 left-2 flex flex-row items-center gap-0.5 rounded-full bg-white px-1">
         <label className="text-xs font-semibold text-black">{label}</label>
-        {required ? <span className="text-xs font-semibold text-[#FF0000]">*</span> : ''}
+        {required ? (
+          <span className="text-xs font-semibold text-[#FF0000]">*</span>
+        ) : (
+          ""
+        )}
       </div>
       <input
         className="mr-1 w-full text-sm placeholder:text-gray-500 focus:outline-none focus:ring-0"
         placeholder={placeholder}
-        id = {label}
+        id={label}
+        {...register(name)}
       />
       {icon != "" ? (
         <Image
@@ -45,11 +66,11 @@ const TextInput = ({textArea = false, label, required = false, placeholder = "",
           height={16}
           className="relative min-h-0 w-4 min-w-0 shrink-0"
         />
-      ) : ""}
-      </div>
-    )
-  
-  )
-}
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 export default TextInput;
