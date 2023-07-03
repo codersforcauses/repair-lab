cc5f8ec3f0442c7b09cd22074fa011744ee32edc
AdrianoLR <larocca.adriano25@gmail.com>
2023-07-03 16:52:19 +0800

    #issue-10-correcting-error

--- src/components/event/index.tsx
+++ src/components/event/index.tsx
@@ -8,13 +8,13 @@ const EventCard: React.FC<{ event: Event }> = ({ event }) => {
         <div className="flex w-full flex-col leading-8 ">
           <p className="text-md font-bold">{event.name}</p>
 
-          <p className="text-lg text-gray-500">
+          <p className="text-gray-500 text-lg">
             Location:
             <span className="text-lg text-[black] ">{event.location}</span>
           </p>
-          <p className="text-lg text-gray-500">
+          <p className="text-gray-500 text-lg">
             Date time
-            <span className="text-lg text-black">
+            <span className="text-black text-lg">
               {event.startDate.toString()} -{event.endDate.toString()}
             </span>
           </p>
@@ -24,4 +24,4 @@ const EventCard: React.FC<{ event: Event }> = ({ event }) => {
   );
 };
 
-export default Cards;
+export default EventCard;
