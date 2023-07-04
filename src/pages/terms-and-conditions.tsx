c4612330b2f67e2e24f711becececd916f30f384
chloetychang <chloetychang@gmail.com>
2023-07-04 17:25:57 +0800

    Basic styling. Pop-up positioning + background needs fixing.

--- src/pages/terms-and-conditions.tsx
+++ src/pages/terms-and-conditions.tsx
@@ -1,8 +1,8 @@
 // Terms and Conditions Pop-up
 
-import React, { useState, ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { Inter } from "next/font/google";
-import { Controller, useForm, SubmitHandler } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,9 +39,14 @@ const Home = () => {
     children: ReactNode;
   }) {
     return trigger ? (
-      <div className="popup">
+      <div className="popup justify-item-center flex h-auto w-auto rounded-md border border-solid">
         <div className="popup-inner">
-          <button className="close-btn">Close</button>
+          <button
+            className="close-btnh w-20 border-spacing-0.5 rounded-md border border-solid text-center text-lg"
+            onClick={() => setShowPopup(false)}
+          >
+            Close
+          </button>
           {children}
         </div>
       </div>
@@ -80,14 +85,105 @@ const Home = () => {
               />
             )}
           />
-          <button onClick={handleshowPopupChange}>
-            {" "}
-            I accept the Terms and Conditions.{" "}
-          </button>
+          <span className="pl-3">
+            I accept the{" "}
+            <button
+              className="text-darkAqua-400"
+              onClick={handleshowPopupChange}
+            >
+              {" "}
+              Terms and Conditions{" "}
+            </button>
+            .
+          </span>
 
           {showPopup && (
             <PopUp trigger={showPopup}>
-              <p>hi</p>
+              <p>
+                <h1 className="text-center text-lg font-bold">
+                  {" "}
+                  House Rules:{" "}
+                </h1>
+                <br></br>
+                <ol className="list-decimal pl-10 pr-10 ">
+                  <li>
+                    Workshop participants carry out the repair themselves
+                    whenever possible, and repair volunteers are on site to help
+                    or provide advice, if necessary.
+                  </li>
+                  <br />
+                  <li>
+                    The work carried out in Repair Lab by repair volunteers is
+                    performed free of charge on a voluntary basis.
+                  </li>
+                  <br />
+                  <li>
+                    The fact that the repairs are being performed by unpaid
+                    volunteers reflects the allocation of risks and limitation
+                    of liability: neither the organisers of Repair Lab nor the
+                    repair volunteers are liable:
+                    <ol className="list-disc">
+                      <li>
+                        for any loss that may result from advice or instructions
+                        concerning repairs,
+                      </li>
+                      <li>for the loss of items handed over for repair,</li>
+                      <li>for indirect or consequential loss,</li>
+                      <li>
+                        for any other kind of loss resulting from work performed
+                        at Repair Lab.
+                      </li>
+                    </ol>
+                  </li>
+                  <br />
+                  <li>
+                    A voluntary donation is always appreciated, to help fund
+                    future Repair Labs.
+                  </li>
+                  <br />
+                  <li>
+                    Any use of new materials such as leads, plugs, fuses, etc.
+                    will be paid for separately.
+                  </li>
+                  <br />
+                  <li>
+                    Visitors offering broken items for repair do so at their own
+                    risk.
+                  </li>
+                  <br />
+                  <li>
+                    Volunteer Mending Mentors offer no guarantee for the repairs
+                    carried out with their help and are not liable if objects
+                    that are repaired at Repair Lab turn out not to work
+                    properly at home.
+                  </li>
+                  <br />
+                  <li>
+                    Repair Volunteers are entitled to refuse to repair certain
+                    objects.
+                  </li>
+                  <br />
+                  <li>
+                    Repair Volunteers are not obliged to reassemble disassembled
+                    appliances that cannot be repaired.
+                  </li>
+                  <br />
+                  <li>
+                    Participants at Repair Lab are solely responsible for the
+                    tidy removal of broken objects that could not be repaired.
+                  </li>
+                  <br />
+                  <li>
+                    Photographs or videos are taken during repair sessions and
+                    may be used for online or in-print news and promotional
+                    materials. If you do not wish to have your photograph used,
+                    please advise the volunteer taking the photos or the
+                    organizers. Please sign to acknowledge that you have read
+                    and accept our House Rules.
+                  </li>
+                </ol>
+                <br></br>
+              </p>
             </PopUp>
           )}
 
@@ -98,7 +194,7 @@ const Home = () => {
         <br></br>
 
         <input
-          className="bg-teal-600 text-white hover:bg-teal-500 m-auto flex h-12 w-60 border-spacing-0.5 justify-center self-center rounded-md border border-solid text-center text-lg"
+          className="text-white hover:bg-teal-500 m-auto flex h-12 w-40 border-spacing-0.5 justify-center self-center rounded-md border border-solid bg-lightAqua-400 text-center text-lg"
           type="submit"
         />
       </form>
