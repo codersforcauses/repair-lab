0e65cfcb9985fa97629492655a0ae80845300039
lilylyi <lilyy395@gmail.com>
2023-07-04 18:20:31 +0800

    add button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,40 @@
+import React from "react";
+
 type Props = {
   color: string;
+  border: string;
+  children?: React.ReactNode;
+  height: string;
+  onClick: () => void;
+  radius: string;
+  width: string;
 };
 
-const index = (props: Props) => {
-  return <div>index</div>;
+const Button: React.FC<Props> = ({
+  color,
+  border,
+  children,
+  height,
+  onClick,
+  radius,
+  width
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: color,
+        border,
+        borderRadius: radius,
+        height,
+        width
+      }}
+    >
+      {children}
+    </button>
+  );
 };
 
-export default index;
+export default Button;
+
+
