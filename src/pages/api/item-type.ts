9e56961966f1723de768dedceff4d13d9b7f8ce1
valtyx13 <81663453+valtyx13@users.noreply.github.com>
2023-07-06 20:55:49 +0800

    Issue 5 submit repair request page (#42)
    * Added Repair Lab logo to public folder (under the folder "images")
    
    * WIP:repair-request endpoint
    
    * Fix: change repair-request test endpoint
    
    * Created a basic layout for the submit repair request page.
    
    * Created a basic layout for repair request page.
    
    * feat: Added RepairRequest and Brand endpoints
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * Fixed some of the syntax through running lint
    
    * Add itemType.ts
    
    * chore: use PrismaClient in lib folder and tidy up imports
    
    * refactor: move db queries to models
    
    * chore: fix validator
    
    * chore: add min letters to description field
    
    * test: fix error
    
    * fix: vitest not recognising absolute import
    
    * item types specified + minor layout changes
    
    * modified code to check whether "data.images" exists
    
    * feat: added event multi-select dropdown + checkbox for Terms and Conditions
    
    * style: deleted unneeded line of code
    
    * WIP: Add image support for repair request api
    
    * Revert image upload in repair request endpoint
    
    * fix: added values for event dropdown options
    
    * style: ran lint
    
    * Remove formidable
    
    * feat: allow s3Keys to be posted in repair request endpoint.
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * feat: working repair request form submission
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * Fix color and add hardcoded events.
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * Add test and reset database
    
    Co-authored-by: Justin Liang <justinliangg@users.noreply.github.com>
    
    * format and lint
    
    * Add defaults for repair request field and fix missing migration
    
    * Fix tests and rename model to use kebab case
    
    * format
    
    * Hardcode seed event data into dropdown
    
    * Address pr comments
    
    * fix incorrect htmlFor
    
    * Export repair request type
    
    ---------
    
    Co-authored-by: chloetychang <chloetychang@gmail.com>
    Co-authored-by: Justin Liang <justin4426@outlook.com>
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    Co-authored-by: wintelestr <133135894+wintelestr@users.noreply.github.com>
    Co-authored-by: Justin Liang <justin.liang@genvis.co>
    Co-authored-by: Justin Liang <justinliangg@users.noreply.github.com>
    Co-authored-by: Dylan To <19729435+dct0@users.noreply.github.com>

---
+++ src/pages/api/item-type.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import itemTypeModel from "@/models/item-type.model";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      getItemTypes(req, res);
+      break;
+    default:
+      return res.status(405).json({
+        error: { message: `Method ${req.method} not allowed` }
+      });
+  }
+}
+
+const getItemTypes = async (req: NextApiRequest, res: NextApiResponse) => {
+  const itemtypes = await itemTypeModel.getAll();
+  return res.status(200).json(itemtypes);
+};
+
+export const config = {
+  api: {
+    externalResolver: true as boolean
+  }
+};
