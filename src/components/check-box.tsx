e69b84e0b51c8155c838b04d8d3f64a3d422c81d
Yinyin Wu <23415578@student.uwa.edu.au>
2023-07-08 20:19:53 +0800

    add documentation

--- src/components/check-box.tsx
+++ src/components/check-box.tsx
@@ -1,27 +1,29 @@
+/*
+This is a component for a checkbox with accompanying text.
+Input:
+  label: The text displayed next to the checkbox
+  href: The URL for the hyperlink
+  hrefLabel: The text displayed for the hyperlink
+Output:
+  A checkbox input with associated text that is compatible with react-hook-form
+*/
+
 import {
   FieldValues,
   useController,
   UseControllerProps
 } from "react-hook-form";
-
 export interface FormProps<T extends FieldValues = FieldValues>
   extends UseControllerProps<T> {
   label?: string;
   href?: string;
-  sampleText?: string;
+  hrefLabel?: string;
 }
 
-/*
-This is a component for a checkbox with accompanying text.
-Input:
-  label: The text displayed next to the checkbox
-Output:
-  A checkbox input with associated text that is compatible with react-hook-form
-*/
 export default function Field_Checkbox<T extends FieldValues = FieldValues>({
   label,
   href,
-  sampleText,
+  hrefLabel,
   ...props
 }: FormProps<T>) {
   const { field } = useController({ ...props });
@@ -32,13 +34,13 @@ export default function Field_Checkbox<T extends FieldValues = FieldValues>({
 
       {label && (
         <label htmlFor={props.name}>
-          {sampleText}
+          {label}
           {href ? (
             <a href={href} className="ml-2 font-bold underline">
-              {label}
+              {hrefLabel}
             </a>
           ) : (
-            label
+            hrefLabel
           )}
         </label>
       )}
