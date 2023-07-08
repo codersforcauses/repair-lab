8ffcd214fc3d4d9a732baf7f3ce544dc5706e173
Juan <juan.selvan77@gmail.com>
2023-07-08 15:00:22 +0800

    added volunteer list in event manager dashboard

---
+++ src/components/repair-request/volunteer-card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function VolunteerCard() {
+  return (
+    <div className="col-span-1 flex w-full justify-between rounded-3xl border bg-[#EEEEEE] p-4">
+      <div className="flex w-full flex-col leading-8 ">
+        <p className="text-md font-bold">james</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/volunteers.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Inter } from "next/font/google";
+import { CiCirclePlus } from "react-icons/ci";
+
+import Navbar from "@/components/Navbar";
+import SearchBar from "@/components/repair-request/SearchBar";
+import SortBy from "@/components/repair-request/SortBy";
+import Sidebar from "@/components/sidebar/index";
+
+import RepairRequestCard from "../components/repair-request/index";
+import VolunteerCard from "../components/repair-request/volunteer-card";
+const inter = Inter({ subsets: ["latin"] });
+
+function getRepairContent() {
+  const repair = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const content = [];
+  for (let i = 0; i < repair.length; i++) {
+    content.push(
+      <div key={i}>
+        <VolunteerCard />
+      </div>
+    );
+  }
+  return content;
+}
+
+export default function Volunteers() {
+  return (
+    <Sidebar>
+    <main className={`ml-80 min-h-screen w-full p-4 ${inter.className}`}>
+      <Navbar />
+      <div className="container">
+        <div className="container mx-auto">
+          <div className="flex justify-end">
+          </div>
+        </div>
+        <div className="grid gap-4 p-4 lg:grid-cols-5 ">
+          {getRepairContent()}
+          <div className="flex w-full items-center justify-center rounded-lg border bg-white p-4">
+            <CiCirclePlus color="#d9d9d9" size={100} />
+          </div>
+        </div>
+        <span className="w-full border-b-[1px] border-gray-200 p-2"></span>
+      </div>
+    </main>
+    </Sidebar>
+  );
+}
+  
