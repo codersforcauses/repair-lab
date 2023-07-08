6c5b36cd281d61d01032e3e6a4f79ee2fc923f40
Juan <juan.selvan77@gmail.com>
2023-07-08 14:25:28 +0800

    add the <main> with styles to fix styling

--- src/pages/repair-request-dashboard.tsx
+++ src/pages/repair-request-dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Inter } from "next/font/google";
 import { CiCirclePlus } from "react-icons/ci";
 
 import Navbar from "@/components/Navbar";
@@ -6,17 +7,32 @@ import SearchBar from "@/components/repair-request/SearchBar";
 import SortBy from "@/components/repair-request/SortBy";
 
 import RepairRequestCard from "../components/repair-request/index";
+const inter = Inter({ subsets: ["latin"] });
+
+
+function getRepairContent() {
+  const repair = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const content = [];
+  for (let i = 0; i < repair.length; i++) {
+    content.push(
+      <div key={i}>
+        <RepairRequestCard />
+      </div>
+    );
+  }
+  return content;
+}
 
 export default function RepairRequest() {
   return (
-    <>
+    <main className={`ml-80 min-h-screen w-full p-4 ${inter.className}`}>
       <Navbar />
       <div className="container">
         <p className="p-4 text-2xl font-bold text-[#6C727F]">Repair requests</p>
         <div className="container mx-auto">
           <div className="flex justify-end">
-            {SortBy()}
-            {SearchBar()}
+            <SortBy />
+            <SearchBar />
           </div>
         </div>
         <div className="grid gap-4 p-4 lg:grid-cols-5 ">
@@ -27,18 +43,7 @@ export default function RepairRequest() {
         </div>
         <span className="w-full border-b-[1px] border-gray-200 p-2"></span>
       </div>
-    </>
+    </main>
   );
 }
-const getRepairContent = () => {
-  const repair = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-  const content = [];
-  for (let i = 0; i < repair.length; i++) {
-    content.push(
-      <div key={i}>
-        <RepairRequestCard />
-      </div>
-    );
-  }
-  return content;
-};
+
