5f1bad1e2a8567996bf80d81b2f26341f1d648a5
lilylyi <lilyy395@gmail.com>
2023-07-08 14:49:16 +0800

    add 'position' to className

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -24,7 +24,7 @@ const Button = ({
   return (
     <button
       onClick={onClick}
-      className={`${color} ${border} ${radius} ${height} ${width} ${textColor}`}
+      className={`${color} ${border} ${radius} ${height} ${width} ${textColor} ${position}`}
     >
       {children}
     </button>
