8d1545a464b76c62e10d55f1ba87e357d12961b6
lilylyi <114378054+lilylyi@users.noreply.github.com>
2023-07-08 13:51:41 +0800

    Issue 37 create a button component (#50)
    * Initial setup for button
    
    * add button component
    
    * updated button
    
    * added textColor
    
    * button adjustment
    
    * prettier
    
    * Issue 5 submit repair request page (#42)
    
    * Added Repair Lab logo to public folder (under the folder "images")
    
    * WIP:repair-request endpoint
    
    * Fix: change repair-request test endpoint
    
    * Created a basic layout for the submit repair request page.
    
    * Created a basic layout for repair request page.
    
    * feat: Added RepairRequest and Brand endpoints
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * Fixed some of the syntax through running lint
    
    * Add itemType.ts
    
    * chore: use PrismaClient in lib folder and tidy up imports
    
    * refactor: move db queries to models
    
    * chore: fix validator
    
    * chore: add min letters to description field
    
    * test: fix error
    
    * fix: vitest not recognising absolute import
    
    * item types specified + minor layout changes
    
    * modified code to check whether "data.images" exists
    
    * feat: added event multi-select dropdown + checkbox for Terms and Conditions
    
    * style: deleted unneeded line of code
    
    * WIP: Add image support for repair request api
    
    * Revert image upload in repair request endpoint
    
    * fix: added values for event dropdown options
    
    * style: ran lint
    
    * Remove formidable
    
    * feat: allow s3Keys to be posted in repair request endpoint.
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * feat: working repair request form submission
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * Fix color and add hardcoded events.
    
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    
    * Add test and reset database
    
    Co-authored-by: Justin Liang <justinliangg@users.noreply.github.com>
    
    * format and lint
    
    * Add defaults for repair request field and fix missing migration
    
    * Fix tests and rename model to use kebab case
    
    * format
    
    * Hardcode seed event data into dropdown
    
    * Address pr comments
    
    * fix incorrect htmlFor
    
    * Export repair request type
    
    ---------
    
    Co-authored-by: chloetychang <chloetychang@gmail.com>
    Co-authored-by: Justin Liang <justin4426@outlook.com>
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    Co-authored-by: wintelestr <133135894+wintelestr@users.noreply.github.com>
    Co-authored-by: Justin Liang <justin.liang@genvis.co>
    Co-authored-by: Justin Liang <justinliangg@users.noreply.github.com>
    Co-authored-by: Dylan To <19729435+dct0@users.noreply.github.com>
    
    * remove button from index
    
    * remove Button import from index
    
    * Using Props to replace React.FC
    
    ---------
    
    Co-authored-by: skyheat <6574274+skyheat@users.noreply.github.com>
    Co-authored-by: valtyx13 <81663453+valtyx13@users.noreply.github.com>
    Co-authored-by: chloetychang <chloetychang@gmail.com>
    Co-authored-by: Justin Liang <justin4426@outlook.com>
    Co-authored-by: valtyx13 <valtyx13@users.noreply.github.com>
    Co-authored-by: wintelestr <133135894+wintelestr@users.noreply.github.com>
    Co-authored-by: Justin Liang <justin.liang@genvis.co>
    Co-authored-by: Justin Liang <justinliangg@users.noreply.github.com>
    Co-authored-by: Dylan To <19729435+dct0@users.noreply.github.com>

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,32 @@
+type Props = {
+  color?: string;
+  border?: string;
+  children?: React.ReactNode;
+  height?: string;
+  onClick?: () => void;
+  radius?: string;
+  width?: string;
+  textColor?: string;
+};
+
+const Button = ({
+  color = "bg-primary-600",
+  border = "border-1",
+  children,
+  height = "h-12",
+  onClick,
+  radius = "rounded-lg",
+  width = "w-1/2",
+  textColor = "text-white"
+}: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${color} ${border} ${radius} ${height} ${width} ${textColor}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
