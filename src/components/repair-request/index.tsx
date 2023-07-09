cb5e067ec0c4a03fee632b3a250d758a5b494829
juan <juan.selvan77@gmail.com>
2023-07-09 12:39:10 +0800

    updated the repair-request component to take props

--- src/components/repair-request/index.tsx
+++ src/components/repair-request/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 
-export default function RepairRequestCard() {
+interface RepairRequestCardProps {
+  description: string;
+  status: string;
+  itemType: string;
+  brand: string;
+  volunteer: string;
+}
+
+export default function RepairRequestCard({props} : {props: RepairRequestCardProps}) {
   return (
     <div className="col-span-1 flex w-full justify-between rounded-3xl border bg-[#EEEEEE] p-4">
       <div className="flex w-full flex-col leading-8 ">
-        <p className="text-md font-bold">Repair Description</p>
+        <p className="text-md font-bold">{props.description}</p>
         <p className="text-lg">
           Status:
-          <span className="rounded-lg bg-green-200 p-1">PENDING</span>
+          <span className="rounded-lg bg-green-200 p-1">{props.status}</span>
         </p>
         <p className="text-lg">
-          Item type: <span>Laptop</span>
+          Item type: <span>{props.itemType}</span>
         </p>
         <p className="text-lg">
-          Brand: <span>Dell</span>
+          Brand: <span>{props.brand}</span>
         </p>
         <p className="text-lg">
-          Volunteer: <span>Volunteer1</span>
+          Volunteer: <span>{props.volunteer}</span>
         </p>
       </div>
     </div>
