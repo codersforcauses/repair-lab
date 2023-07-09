22fc3f41b05953f4c10ffc546ac1e98e4b9cd61a
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-07-09 11:31:57 +0800

    fix button

--- src/components/custombutton-large.tsx
+++ src/components/custombutton-large.tsx
@@ -5,12 +5,13 @@ interface CustomButtonProps extends HTMLAttributes<HTMLButtonElement> {
   text: string;
 }
 
-const CustomButton = ({ onClick, text }: CustomButtonProps) => {
+const CustomButton = ({ onClick, text, ...props }: CustomButtonProps) => {
   return (
     <button
       type="submit"
       onClick={onClick}
       className="w-full cursor-pointer rounded-md bg-primary-600 py-2 font-bold text-white"
+      {...props}
     >
       {text}
     </button>
