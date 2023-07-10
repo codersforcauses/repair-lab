abd91f88e14a946cc9f0e8b7508ef49117e1e442
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2023-07-10 16:16:29 +0800

    added header and required as functional props

--- src/components/dropdown-headless.tsx
+++ src/components/dropdown-headless.tsx
@@ -8,6 +8,9 @@ interface Props {
   options: { id: number; text: string }[];
   width?: number;
   height?: number;
+  required?: boolean;
+  placeholder: string;
+  header?: string;
 }
 
 function classNames(...classes: any) {
@@ -19,20 +22,38 @@ export default function DropDown({
   selected,
   setSelected,
   width = 56,
-  height = 24
+  height = 20,
+  required = false,
+  placeholder = "-select-",
+  header = ""
 }: Props) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button
-          className={`flex justify-between w-${width} h-${height} gap-x-1.5 rounded-md bg-white px-3 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-grey-50`}
+          className={`flex justify-between w-${width} h-${height} gap-x-1.5 overflow-hidden rounded-md bg-white px-3 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-grey-50`}
         >
-          {selected === "" ? (
-            <span className="opacity-70">-select-</span>
+          {header !== "" ? (
+            required === false ? (
+              <span className="justify center absolute -top-3 flex items-center bg-white px-2 py-1 text-xs text-grey-700">
+                {header}
+                
+              </span>
+            ) : (
+              <span className="justify center absolute -top-3 flex items-center bg-white px-2 py-1 text-xs text-grey-700">
+                {header}<div className="text-red-500">*</div>
+              </span>
+            )
           ) : (
-            selected
+            ""
           )}
-          <div className="-mr-1 h-5 w-5 text-gray-400">{"\u2304"}</div>
+
+          {selected === "" ? (
+            <span className="opacity-70">Item Type</span>
+          ) : (
+            <p className="truncate text-grey-700">{selected}</p>
+          )}
+          <div className="-mr-1 h-5 w-5 text-grey-700">{"\u2304"}</div>
           {/* <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" /> */}
         </Menu.Button>
       </div>
--- src/pages/field-dropdown-tester.tsx
+++ src/pages/field-dropdown-tester.tsx
@@ -8,7 +8,7 @@ export default function Tester() {
     <main>
       <h1>This is the dropdown tester page</h1>
       <p>content above</p>
-      <DropDownField
+      {/* <DropDownField
         selected={selected}
         setSelected={setSelected}
         options={[
@@ -45,46 +45,50 @@ export default function Tester() {
             text: "A really really long option for the purpose of testing"
           }
         ]}
-      />
+      /> */}
       <div className="flex justify-center">
-        <Dropdown 
-        selected={selected}
-        setSelected={setSelected}
-        // width={80}
-        options={[
-          {
-            id: 0,
-            text: "Option1"
-          },
-          {
-            id: 1,
-            text: "Option2"
-          },
-          {
-            id: 2,
-            text: "Option3"
-          },
-          {
-            id: 3,
-            text: "LongerOption4"
-          },
-          {
-            id: 4,
-            text: "EvenLongerOption5"
-          },
-          {
-            id: 5,
-            text: "Option6"
-          },
-          {
-            id: 6,
-            text: "Option7"
-          },
-          {
-            id: 7,
-            text: "A really really long option for the purpose of testing"
-          }
-        ]}/>
+        <Dropdown
+          selected={selected}
+          setSelected={setSelected}
+          // width={80}
+          placeholder="Select an Item"
+          header="header"
+          required={true}
+          options={[
+            {
+              id: 0,
+              text: "Option1"
+            },
+            {
+              id: 1,
+              text: "Option2"
+            },
+            {
+              id: 2,
+              text: "Option3"
+            },
+            {
+              id: 3,
+              text: "LongerOption4"
+            },
+            {
+              id: 4,
+              text: "EvenLongerOption5"
+            },
+            {
+              id: 5,
+              text: "Option6"
+            },
+            {
+              id: 6,
+              text: "Option7"
+            },
+            {
+              id: 7,
+              text: "A really really long option for the purpose of testing"
+            }
+          ]}
+        />
       </div>
       <p>content below</p>
       <p className="flex justify-center">more content below</p>
