a0db4e637c1af475d51dedbafc9ae92330cbeed7
skyheat <6574274+skyheat@users.noreply.github.com>
2023-07-11 02:19:30 +0800

    refactored button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,33 +1,33 @@
-type Props = {
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string;
   border?: string;
-  children?: React.ReactNode;
   height?: string;
-  onClick?: () => void;
   radius?: string;
   width?: string;
   textColor?: string;
   textSize?: string;
   hover?: string;
   position?: string;
-};
+}
 
 export default function Button({
   color = "bg-primary-600",
   border = "border-1",
-  children,
   height = "h-12",
-  onClick,
   radius = "rounded-lg",
   width = "w-full",
   textColor = "text-white",
   textSize = "text-lg",
   hover = "hover:bg-primary-800",
-  position = "mx-auto"
-}: Props) {
+  position = "mx-auto",
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      onClick={onClick}
+      {...props}
       className={`${color} ${border} ${radius} ${height} ${width} ${textColor} ${textSize} ${hover} ${position}`}
     >
       {children}
