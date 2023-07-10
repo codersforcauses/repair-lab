dfb1e30034d36b13536d00a91067d805bb2e4db6
ellenxiao0412@gmail.com <ellenxiao0412@gmail.com>
2023-07-10 17:49:59 +0800

    deleted unused imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "@/styles/globals.css";
-import "@/styles/repair.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
