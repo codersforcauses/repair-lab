cbfd4310051d429966973a385aeebde0a4a29ead
Stanley Natalius <131582712+yelnats8@users.noreply.github.com>
2023-07-10 22:24:51 +0800

    Issue 26 create text related reusable components (#40)
    * Made basic text input component, not working yet
    
    * Basic layout + form data in console
    
    * Added text area as optional input field
    
    * Add reusable field (label+input) component for rhf
    
    * removed unused import statements
    
    * Added formContext, now works w/ react hook form
    
    * Field component
    
    * Combined Stanley's work with Ellen's
    
    * Combined Stanley's work with Ellen's
    
    * added basic documentation
    
    * updated documentation and added in suggestions
    
    * fixed css not extending base Tailwind
    
    * reordered optional placeholder and icon logic
    
    * fixed tailwind.config.js not passing prettier test
    
    * updated naming to kebab-case
    
    * split the label into its own component
    
    * added a component for displaying error messages
    
    * changed radio input values to "true" and "false"
    
    * function to PascalCase, changed radio btn margin
    
    * improved styling and structure for field-input
    
    * improved styling and structure for field-radio
    
    * moved component directory
    
    * added some info in form_testing
    
    * removed repair.css styling
    
    ---------
    
    Co-authored-by: ellenxiao0412@gmail.com <ellenxiao0412@gmail.com>
    Co-authored-by: Dylan To <19729435+dct0@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -75,11 +75,6 @@ module.exports = {
         }
       }
     },
-    backgroundImage: {
-      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      "gradient-conic":
-        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
-    }
-  },
-  plugins: []
+    plugins: []
+  }
 };
