a4ce5d6419f7879801e1a873828b98fad07a7a53
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2023-07-11 10:16:32 +0800

    cleaned up tester page

--- src/pages/field-dropdown-tester.tsx
+++ src/pages/field-dropdown-tester.tsx
@@ -7,44 +7,6 @@ export default function Tester() {
     <main>
       <h1>This is the dropdown tester page</h1>
       <p>content above</p>
-      {/* <DropDownField
-        selected={selected}
-        setSelected={setSelected}
-        options={[
-          {
-            id: 0,
-            text: "Option1"
-          },
-          {
-            id: 1,
-            text: "Option2"
-          },
-          {
-            id: 2,
-            text: "Option3"
-          },
-          {
-            id: 3,
-            text: "LongerOption4"
-          },
-          {
-            id: 4,
-            text: "EvenLongerOption5"
-          },
-          {
-            id: 5,
-            text: "Option6"
-          },
-          {
-            id: 6,
-            text: "Option7"
-          },
-          {
-            id: 7,
-            text: "A really really long option for the purpose of testing"
-          }
-        ]}
-      /> */}
       <div className="flex justify-center">
         <Dropdown
           selected={selected}
