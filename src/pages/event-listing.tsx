56a5d76f905e12b83240d785690ce2774b195944
Joel Wildman <79435468+BunchOfCapital@users.noreply.github.com>
2023-07-12 15:28:35 +0800

    added and styled modal with no content

--- src/pages/event-listing.tsx
+++ src/pages/event-listing.tsx
@@ -1,10 +1,26 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { faChevronDown, faChevronUp, faSearch, faCog } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp, faSearch, faCog, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Event } from "@prisma/client";
 
+import { useForm } from "react-hook-form";
+
+import { Dialog } from '@headlessui/react'
+
+
+
+
+async function getEvent(eventName: string) {
+  return await prisma.event.findUnique({
+    where: {name: eventName}
+  })
+}
+
 function Table() {
+  const [modalActive, toggleModal] = useState(false)
+
+
   function formatDate(dateString: string): string {
     const actualDate = new Date(dateString);
     const day = actualDate.getDate().toString().padStart(2, "0");
@@ -36,17 +52,24 @@ function Table() {
   ];
 
   // Whenever the sortKey or sortMethod changes, the useEffect hook will run
-  useEffect(() => {
-    const params = new URLSearchParams();
-    params.append('sortKey', sortKey);
-    params.append('sortMethod', sortMethod);
+  // useEffect(() => {
+  //   const params = new URLSearchParams();
+  //   params.append('sortKey', sortKey);
+  //   params.append('sortMethod', sortMethod);
   
-    fetch(`/api/get_events?${params.toString()}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setEventData(data);
-      });
-  }, [sortKey, sortMethod]);
+  //   fetch(`/api/get_events?${params.toString()}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setEventData(data);
+  //     });
+  // }, [sortKey, sortMethod]);
+
+
+  //will toggle modal visibility for editing events
+
+  useEffect(() => {
+
+  }, [modalActive]);
   
 
   function handleButtonClick(key: string) {
@@ -87,6 +110,14 @@ function Table() {
     setSortKey(key);
   }
 
+  function optionsModal(name: string) {
+    return (
+      <div className="absolute align-middle self-center w-1/2 h-2/3 border-slate-700 bg-50 rounded-md">
+        <h1> MODAL TIME </h1>
+      </div>
+      );
+  }
+
   function SortOptions() {
     // Basic functionality for sorting, styling incomplete
     return (
@@ -104,6 +135,8 @@ function Table() {
 
   return (
     <div>
+
+
       <div
         style={{
           display: "flex",
@@ -121,11 +154,41 @@ function Table() {
         <h1 style={{ marginLeft: "10px", color: "rgb(175, 177, 182)", fontWeight: 600, fontSize: "24px" }}>Event Listings</h1>
       </div>
 
+       {/*options modal*/}
+      <div>
+        <button onClick={() => toggleModal(true)}>Open Modal</button>
+          <Dialog open={modalActive} onClose={() => toggleModal(false)}
+          className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Title className=" flow-root border-b-[1px] border-slate-300 p-4 bg-lightAqua-300 font-semibold">
+                <span className="float-left"> Edit Event Details </span>
+                <button onClick={() => toggleModal(false)} className="hover:bg-lightAqua-500 rounded-full items-center w-6 h-6 float-right">
+                  <FontAwesomeIcon className="align-middle text-xl align-text-top" icon={faXmark} />
+                </button>
+
+              </Dialog.Title>
+              <Dialog.Description className="p-3">
+                Select each field below to change their contents
+              </Dialog.Description>
+
+              <p>
+                
+              </p>
+
+              <div className=" border-t-[1px] border-slate-200 ">
+                <button onClick={() => toggleModal(false)} className="bg-transparent hover:bg-lightAqua-500 text-lightAqua-500 font-light hover:text-white py-1 px-2 border border-lightAqua-500 hover:border-transparent rounded m-1 text-sm">
+                  Accept
+                </button>
+                <button onClick={() => toggleModal(false)} className="bg-transparent hover:bg-lightAqua-500 text-lightAqua-500 font-light hover:text-white py-1 px-2 border border-lightAqua-500 hover:border-transparent rounded m-1 text-sm">
+                  Cancel
+                </button>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+      </div>
+
       {/* Basic functionality for sorting, styling incomplete */}
-
-
-
-
 
       <div className="flex justify-center">
         <div className="w-5/12 p-4 bg-red-500  relative">
@@ -150,10 +213,10 @@ function Table() {
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="container">
+        <div className="container ">
           <table className="table-auto">
           <thead>
-              <tr className="pb-10 text-left hover:bg-gray-100">
+              <tr className="pb-10 text-left">
                 <th className="pl-5"> {headers[0].label} <ToggleChevron column="0"/> </th>
                 <th> {headers[1].label} <ToggleChevron column="1"/> </th>
                 <th> {headers[2].label} <ToggleChevron column="2"/> </th>
