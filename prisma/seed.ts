63e189930df3b6c482a1cbaeb171c4c5907bd054
dominictdavies <dominictdavies@gmail.com>
2023-07-14 02:10:21 +0800

    Random repair requests

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { faker } from "@faker-js/faker";
-import { Brand, Event, ItemType, PrismaClient } from "@prisma/client";
+import { Brand, Event, ItemType, PrismaClient, RepairRequest } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function deleteAllData() {
@@ -23,6 +23,7 @@ async function deleteAllData() {
 
 async function createItemTypes(itemTypeNames: string[]) {
   const itemTypes: ItemType[] = [];
+
   for (const name of itemTypeNames) {
     const itemType = await prisma.itemType.create({
       data: { name },
@@ -30,11 +31,13 @@ async function createItemTypes(itemTypeNames: string[]) {
     itemTypes.push(itemType);
     console.log(itemType);
   }
+
   return itemTypes;
 }
 
 async function createBrands(brandNames: string[]) {
   const brands: Brand[] = [];
+
   for (const name of brandNames) {
     const brand = await prisma.brand.create({
       data: { name },
@@ -42,22 +45,23 @@ async function createBrands(brandNames: string[]) {
     brands.push(brand);
     console.log(brand);
   }
+
   return brands;
 }
 
-async function createRandomEvents(count: number, itemTypeNames: string[]) {
+async function createRandomEvents(count: number, itemTypes: ItemType[]) {
   const events: Event[] = [];
+
   for (let i = 0; i < count; i++) {
     const event = await prisma.event.create({
       data: {
         createdBy: faker.person.fullName(),
         name: faker.commerce.productName(),
         location: faker.location.street(),
-        description:
-          faker.lorem.sentence(),
+        description: faker.lorem.sentence(),
         volunteers: [faker.person.fullName()],
         event: {
-          connect: { name: faker.helpers.arrayElement(itemTypeNames) }
+          connect: { name: faker.helpers.arrayElement(itemTypes).name }
         },
         startDate: new Date(2023, 5, 28, 15, 30, 0, 0),
         endDate: new Date(2023, 6, 1, 15, 30, 0, 0)
@@ -66,53 +70,63 @@ async function createRandomEvents(count: number, itemTypeNames: string[]) {
     events.push(event);
     console.log(event);
   }
+
   return events;
 }
 
-async function createRandomRepairRequest() {
-  const repairRequest = await prisma.repairRequest.create({
-    data: {
-      createdBy: "Alice",
-      assignedTo: "Cheshire Cat",
-      event: {
-        connect: { id: "event1.id" }
+async function createRandomRepairRequests(count: number, events: Event[], itemTypes: ItemType[]) {
+  const repairRequests: RepairRequest[] = [];
+
+  for (let i = 0; i < count; i++) {
+    const repairRequest = await prisma.repairRequest.create({
+      data: {
+        createdBy: faker.person.fullName(),
+        assignedTo: faker.person.fullName(),
+        event: {
+          connect: { id: faker.helpers.arrayElement(events).id }
+        },
+        status: faker.helpers.arrayElement(["PENDING", "COMPLETED"]),
+        description: faker.lorem.sentence(),
+        comment: faker.lorem.sentence(),
+        itemBrand: faker.helpers.arrayElement(itemTypes).name,
+        requestDate: new Date(),
+        updatedAt: new Date(),
+        item: {
+          connect: { name: faker.helpers.arrayElement(itemTypes).name }
+        },
+        thumbnailImage: "https://tinyurl.com/broken-clock-sad",
+        images: {
+          create: [
+            {
+              s3Key: "https://tinyurl.com/broken-clock-sad"
+            },
+            {
+              s3Key: "https://tinyurl.com/broken-clock-sad"
+            }
+          ]
+        }
       },
-      status: "PENDING",
-      description: "Clock stopped ticking",
-      comment: "Clock starts to tick for 3 seconds after it has been shaken.",
-      itemBrand: "Wonderland",
-      requestDate: new Date(),
-      updatedAt: new Date(),
-      item: {
-        connect: { name: "Clock" }
-      },
-      thumbnailImage: "https://tinyurl.com/broken-clock-sad",
-      images: {
-        create: [
-          {
-            s3Key: "https://tinyurl.com/broken-clock-sad"
-          },
-          {
-            s3Key: "https://tinyurl.com/broken-clock-sad"
-          }
-        ]
-      }
-    },
-  });
-  console.log(repairRequest);
+    });
+    repairRequests.push(repairRequest);
+    console.log(repairRequest);
+  }
+
+  return repairRequests;
 }
 
 async function main() {
   faker.seed(0);
+  await deleteAllData();
 
   const itemTypeNames: string[] = ["Clock", "Bike", "Computer"];
   const brandNames: string[] = ["Wonderland", "BikeBrand", "OtherBikeBrand"];
+  const eventCount = 10;
+  const repairRequestCount = 15;
 
-  await deleteAllData();
-
-  const itemTypes = await createItemTypes(itemTypeNames);
-  const brands = await createBrands(brandNames);
-  const events = await createRandomEvents(30, itemTypeNames);
+  const itemTypes: ItemType[] = await createItemTypes(itemTypeNames);
+  const brands: Brand[] = await createBrands(brandNames);
+  const events: Event[] = await createRandomEvents(eventCount, itemTypes);
+  const repairRequests: RepairRequest[] = await createRandomRepairRequests(repairRequestCount, events, itemTypes);
 }
 
 main()
