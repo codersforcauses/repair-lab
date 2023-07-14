b7553850d83588e031f974c3fb41ead4d7ac224b
Stanley Natalius <22915411@student.uwa.edu.au>
2023-07-14 18:17:56 +0800

    added the assignee badge(?) (the bit where you can see the first name, last name, and their avatar)

---
+++ src/components/Cards/assignee-badge.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  firstName?: string;
+  lastName?: string;
+  avatar?: string;
+};
+
+export default function AssigneeBade({ firstName, lastName, avatar }: Props) {
+  return (
+    <div className="rounded-lg bg-slate-300 p-2">
+      <div>
+        <div className="flex flex-row gap-2">
+          <div className="h-8 w-8 self-center rounded-full bg-blue-500"></div>
+          <div className="flex flex-col ">
+            <p className="text-xs font-bold">{firstName}</p>
+            <p className="text-xs">{lastName}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Cards/card.tsx
+++ src/components/Cards/card.tsx
@@ -1,3 +1,4 @@
+import AssigneeBade from "@/components/Cards/assignee-badge";
 import StatusPill from "@/components/Cards/status-pill";
 
 type Props = {
@@ -18,14 +19,17 @@ export default function Card({ title, description, image, status }: Props) {
       onClick={handleClick}
       onKeyDown={handleClick}
       role="presentation"
-      className="w-64 flex-col overflow-clip rounded-lg bg-slate-200 shadow-2xl transition hover:-translate-y-0.5 hover:cursor-pointer hover:bg-slate-100"
+      className="w-72 flex-col overflow-clip rounded-lg bg-slate-200 shadow-2xl transition hover:-translate-y-0.5 hover:cursor-pointer hover:bg-slate-100"
     >
       <div className="flex justify-center">
         <img src={image} alt="" className="object-fit max-h-32 w-full" />
       </div>
       <div className="p-2">
-        <div className="mb-1 text-xl font-bold">
-          <h3>{title}</h3>
+        <div className="mb-1 flex flex-row items-center justify-between">
+          <h3 className=" t self-start text-xl font-bold">{title}</h3>
+          <div className="">
+            <AssigneeBade firstName="Bambang" lastName="Johnson" avatar="yes" />
+          </div>
         </div>
         <div className="mb-3 h-32 overflow-auto text-sm font-light">
           <p>{description}</p>
