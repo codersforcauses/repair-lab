421bb3c6b28bc2a7af08d0671a04bde6581f86d5
wintelestr <133135894+wintelestr@users.noreply.github.com>
2023-07-15 13:05:45 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 
-import Modal from "@/components/Modal/index";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -9,7 +7,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Modal />
+      <h1>Hello!</h1>
     </main>
   );
 }
