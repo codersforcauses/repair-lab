cb69241413a5895339fd528af3671c48b4bf175b
Vyvienne Bautista <vyviennefaye@gmail.com>
2023-07-15 13:43:46 +0800

    Completed section 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { Inter } from "next/font/google";
 import Image from "next/image";
 
-import Button from "@/components/Button";
-
+import Button from "@/components/Button/index";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="relative">
+    <div className="">
       <div className="h-screen w-screen bg-blue-100">
         <div className="relative h-1/3 w-full">
           <Image
@@ -19,7 +18,7 @@ export default function Home() {
             className="object-cover object-center"
           />
         </div>
-        <div className="flex w-full -translate-y-1/2 transform justify-center">
+        <div className="flex w-full -mt-16 justify-center relative z-10">
           <Image
             src="/images/repair_lab_logo.png"
             alt="Greyscale picture of water animal"
@@ -28,9 +27,9 @@ export default function Home() {
             className="h-28 w-28"
           />
         </div>
-        <div className=" -translate-y-12">
-          <div className="mb-2 text-center text-3xl font-bold mt-5">About Us</div>
-          <div className="pl-10 pr-10 pt-10 text-sm text-center">
+        <div className="">
+          <div className="mb-10 text-center text-3xl font-bold mt-5">About Us</div>
+          <div className="pl-10 pr-10 text-sm text-center">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text ever
             since the 1500s, when an unknown printer took a galley of type and
@@ -44,11 +43,13 @@ export default function Home() {
         </div>
       </div>
       <div>page 2</div>
-      <div className="flex-col h-9 bg-white pb-10 -translate-y-1/2 -mb-5">
-        <div className="text-center text-2xl font-bold mt-10">
+      <div className="flex-col h-auto bg-white">
+        <div className="text-center text-3xl font-bold mt-16 mb-10">
           Submit item for repair
         </div>
-        <Button aria-label="button" className="ml-10 mr-10 h-10 mb-5"> New Request </Button>
+        <div className="h-30 pr-10 pl-10 mt-5 mb-16">
+        <Button aria-label="button"> New Request </Button>
+        </div>
       </div>
     </div>
   );
