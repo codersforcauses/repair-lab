c5a1411ce00ad8eebcf2cceeb7bb84030d5aa9bb
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-07-15 14:38:46 +0800

    create event-form schema

--- src/schema/event-form.ts
+++ src/schema/event-form.ts
@@ -3,16 +3,12 @@ import { z } from "zod";
 export const eventFormPostSchema = z.object({
   name: z.string().nonempty(),
   location: z.string().nonempty(),
-  startDate: z.date({
-    // or z.string().nonempty(), ?
-    required_error: "Please select a date and time.",
-    invalid_type_error: "Please input a valid date and time."
+  startDate: z.date().refine((value) => value instanceof Date, {
+    message: "Please select a valid date and time.",
   }),
-  endDate: z.date({
-    // or z.string().nonempty(), ?
-    required_error: "Please select a date and time.",
-    invalid_type_error: "Please input a valid date and time."
+  endDate: z.date().refine((value) => value instanceof Date, {
+    message: "Please select a valid date and time.",
   }),
   eventType: z.string().nonempty(),
-  volunteers: z.string().nonempty()
+  volunteers: z.string().nonempty(),
 });
