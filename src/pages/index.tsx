08bdb9ba858f8cb0956d44d4f9a0ceecbbd043fc
Vyvienne Bautista <vyviennefaye@gmail.com>
2023-07-15 14:39:58 +0800

    Escaped apostrophe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,14 +28,14 @@ export default function Home() {
         </div>
         <div className="pl-10 pr-10 text-center text-sm">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&apos;s standard dummy
-          text ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum.
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s, when an unknown printer took a galley of type
+          and scrambled it to make a type specimen book. It has survived not
+          only five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
         </div>
       </div>
       <div className="flex flex-col items-center justify-center bg-lightAqua-300 p-10">
