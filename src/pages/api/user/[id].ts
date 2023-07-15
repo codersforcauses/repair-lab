da276e7e81864ebc7c7a6ad30e8c1b98e708dd33
valtyx13 <20164481@student.curtin.edu.au>
2023-07-15 14:54:55 +0800

    Add API endpoints for GET user list and user by id

---
+++ src/pages/api/user/[id].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse, PageConfig } from "next";
+
+import UserService from "@/services/user";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      getUser(req, res);
+      break;
+    default:
+      return res.status(405).json({
+        error: { message: `Method ${req.method} not allowed` }
+      });
+  }
+}
+
+const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query
+  const userService = new UserService();
+  const users = await userService.getUser(id as string);
+
+  return res.status(200).json(users);
+};
+
+export const config: PageConfig = {
+  api: {
+    externalResolver: true
+  }
+};
---
+++ src/services/user.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server/clerkClient";
+
+interface IUserService {
+  getAll(): Promise<User[]>;
+  getUser(userId: string): Promise<User>
+}
+
+class UserService implements IUserService {
+  async getAll(): Promise<User[]> {
+    return await clerkClient.users.getUserList();
+  }
+
+  async getUser(userId: string): Promise<User> {
+    return await clerkClient.users.getUser(userId);
+  }
+}
+
+export default UserService;
