b1eda7e9d8e7e1939bed32f56917d31993930d1b
Vyvienne Bautista <vyviennefaye@gmail.com>
2023-07-15 11:30:18 +0800

    Marked start of section 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="relative">
       <div className="h-screen w-screen">
-        <div className="h-1/4 w-full">
+        <div className="h-1/3 w-full">
           <Image
             src="/images/landing.jpg"
             alt="Greyscale picture of water animal"
@@ -41,18 +41,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-screen w-screen">
-        <div className="">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s
-          with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
-        </div>
+      {/* PAGE 2 START */}
+      <div>
+      </div>
       </div>
     </div>
   );
