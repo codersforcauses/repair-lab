be279bfffaf1f95487ce374f0aa0a525067facf0
Yinyin Wu <23415578@student.uwa.edu.au>
2023-07-15 13:59:38 +0800

    create page layout

--- src/pages/repair-request-list.tsx
+++ src/pages/repair-request-list.tsx
@@ -1,29 +1,24 @@
 import Image from "next/image";
 
-
 export default function ListCard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-4">
+      {/* Logo of Repair Lab, which links to the main website. */}
+      <picture>
+        <a href="https://repairlab.myfreesites.net/" target="_blank">
+          <Image
+            src="/images/repair_lab_logo.jpg"
+            alt="Repair Labs Logo"
+            width={80}
+            height={80}
+          />
+        </a>
+      </picture>
 
-  
-  return(
-  <main
-    className={`flex min-h-screen flex-col items-center gap-4 ${inter.className}`}
-  >
-    {/* Logo of Repair Lab, which links to the main website. */}
-
-    <picture>
-      <a href="https://repairlab.myfreesites.net/" target="_blank">
-        <Image
-          src="/images/repair_lab_logo.jpg"
-          alt="Repair Labs Logo"
-          width={80}
-          height={80}
-        />
-      </a>
-    </picture>
-
-    {/* Heading of the Page */}
-
-    <h1 className="text-xl font-bold"> Submit a Repair Request</h1>
+      {/* Heading of the Page */}
+      <h1 className="text-xl font-bold"> Repair Request List</h1>
     </main>
-    )
+  );
 }
+
+
