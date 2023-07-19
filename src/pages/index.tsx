7043f86f54be0e22f044e147a30e74feff6a7354
chloetychang <chloetychang@gmail.com>
2023-07-19 15:06:20 +0800

    Replaced some placeholders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <div className="bg-lightAqua-100">
       <div className="relative h-44 w-full">
         <Image
-          src="/images/landing.jpg"
-          alt="Greyscale picture of water animal"
+          src="/images/sewing_box.jpg"
+          alt="box of sewing equipment"
           fill={true}
           className="object-cover object-center"
         />
@@ -16,25 +16,84 @@ export default function Home() {
       <div className="relative z-10 -mt-16 flex w-full justify-center">
         <Image
           src="/images/repair_lab_logo.png"
-          alt="Greyscale picture of water animal"
+          alt="Repair Labs Logo"
           width={112}
           height={112}
         />
       </div>
       <div className="mb-16">
-        <div className="mb-10 mt-5 text-center text-3xl font-bold">
-          About Us
-        </div>
-        <div className="pl-10 pr-10 text-center text-sm">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+        <div className="mb-8 mt-5 text-center text-3xl font-bold">About Us</div>
+        <div className="text-ml flex flex-col pl-10 pr-10">
+          <div className="flex flex-col justify-center">
+            <div className="relative flex w-full justify-center">
+              <Image
+                src="/images/generalToy.jpeg"
+                alt="box of sewing equipment"
+                width={400}
+                height={300}
+              />
+            </div>
+
+            <ul className="tlex grid list-none justify-center gap-y-2 pr-4">
+              <li className="mt-6">
+                Repair Lab is a zero budget volunteer group with 100%
+                volunteers, consisting of a group of locals who care for their
+                community and the environment.
+              </li>
+
+              <li>
+                People who think that we can do more than only recycle and buy
+                new.
+              </li>
+
+              <li>
+                Who think that we can not only make an impact by repairing stuff
+                but also learning repair skills and enjoy doing so.
+              </li>
+
+              <li className="border-b-2 border-darkAqua-600 pb-8">
+                We hope that there will be many Repair Labs in Perth and the
+                rest of WA following after our pilot in October 2017.
+              </li>
+            </ul>
+
+            <div className="relative mt-8 flex w-full justify-center">
+              <Image
+                src="/images/Repair_Lab_bikerepair.jpg"
+                alt="bike repair"
+                width={400}
+                height={300}
+              />
+            </div>
+
+            <ul className="tlex grid list-none justify-center gap-y-2 pr-4">
+              <li className="mt-6">
+                We are always on the lookout for more volunteers.
+              </li>
+
+              <li>
+                Especially people with electrical knowledge are on high demand.
+              </li>
+
+              <li>
+                But also people who can repair toys, bikes, furniture, clothing,
+                jewellery, curtains, household items, garden tools, etc. etc.
+              </li>
+
+              <li>
+                Sometimes it is not only repair skills as well as care
+                instructions. Maybe you can teach others how to keep your tools
+                in mint condition, or how to crochet.
+              </li>
+
+              <li>
+                If you are handy or have a good repair skills and eager to share
+                them with your community, please contact us.
+              </li>
+
+              <li>Wilma & Flavia</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center bg-lightAqua-300 p-10">
