d611202ad73e1525a80a7b06536c0c141f3e2853
Nicholas Choong <nicholascwc1997@gmail.com>
2023-07-24 18:16:14 +0800

    added zod validation for files

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -5,52 +5,72 @@ import {
   PutObjectOutput,
   S3Client
 } from "@aws-sdk/client-s3";
+import { z } from "zod";
 
 export const config = {
   runtime: "edge"
 };
 
-// const Files = z.object({
-//   files: z.instanceof(File)
-// });
+const fileSchema = z
+  .custom<File>((val) => val instanceof File, "Please upload a file")
+  .refine(
+    (file) => file.size < 10 * 1024 * 1024,
+    "File size must be less than 10MB"
+  )
+  .refine(
+    (file) => file.type.match(/^image\/(png|jpeg|jpg|webp)$/),
+    "File type must be png, jpeg, jpg or webp"
+  )
+  .refine((file) => file.name.length != 0, "File name must not be empty");
+
+const reqSchema = z.array(fileSchema);
 
 export default async function handler(req: NextRequest) {
   switch (req.method) {
     case "POST":
       try {
-        const data = await req.formData();
-        const files = data.getAll("files") as File[];
-
-        if (!Array.isArray(files)) {
+        const contentType = req.headers.get("content-type");
+        if (!contentType || !contentType.startsWith("multipart/form-data")) {
           return NextResponse.json({
             status: 400,
-            error: "Files must be an array"
+            error: "Request must be multipart/form-data"
           });
         }
 
-        const responds: PutObjectOutput[] = [];
+        const data = await req.formData();
+        const rawFiles = data.getAll("files");
+
+        const files = reqSchema.parse(rawFiles);
+
+        const responses: PutObjectOutput[] = [];
         for (const file of files) {
-          const respond = await sendToBucket(file);
-          responds.push(respond);
+          const response = await sendToBucket(file);
+          responses.push(response);
         }
 
         return NextResponse.json({
           status: 200,
           message: "Images uploaded successfully",
-          responds: responds
+          responds: responses
         });
       } catch (err) {
         console.error("Error in uploading images!", err);
         return NextResponse.json({
           status: 400,
-          error: "Error in uploading images!"
+          error: `Error in uploading images!, ${err}`
         });
       }
+    default:
+      return NextResponse.json({
+        status: 405,
+        error: "POST requests only"
+      });
   }
 }
 
 async function sendToBucket(file: File) {
   try {
+    console.log(typeof file);
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     const fileParams: PutObjectCommandInputType = {
