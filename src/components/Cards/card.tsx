ddbc34913803fa213fe3156779f65438600eedcd
Adriano La Rocca <60787833+AdrianoLR@users.noreply.github.com>
2023-07-26 21:42:49 +0800

    Issue 10 create event manager dashboard (#41)
    * added schema
    
    * #issue-10-creating-dashboard
    
    * #issue-10-finalizing-the-dashboard-layout-v1
    
    * #issue-10-fetching-data-not-working
    
    * #issue-10-finalising-layout-repair-request
    
    * #issue-10-correcting-error
    
    * #issue-10-run-prettier
    
    * #issue-10-removing-lines
    
    * change Image src to string
    
    * change Logo src to string
    
    * fix src string, formatted with prettier
    
    * #issue-10-simple-api-tests
    
    * #issue-10-deleting-repair-request-test
    
    * #issue-10-run-prettier
    
    * #issue-10-fixing-test-route
    
    * #issue-10-discarting-tests-alterations
    
    * add the <main> with styles to fix styling
    
    * format with prettier
    
    * removed <Sidebar> from _app
    
    * added volunteer list in event manager dashboard
    
    * add Sidebar to the repair-request-dash component
    
    * fixed sidebar to look more like figma
    
    * added volunteer title
    
    * updated the repair-request component to take props
    
    * connected RepairRequest columnto eventmanager page
    
    * removed unrequired imports
    
    * formatting with prettier, remove console.logs
    
    * #issue-10-merging-branches
    
    * fixed import paths for sidebar, header
    
    * made endpoint to get volunteers per event from DB
    
    * updated volunteers page to use new endpoint
    
    * fixed header component name
    
    * #issue-10-changing-names-and-places
    
    * fixed error (by removing async)
    
    * #issue-10-changing-layout-with-thumbnail-and-colors
    
    * #issue-10-changing-folder-to-kebal-case
    
    * #issue-10-changing-error-in-volunteer-card
    
    * import interface from component page, not redefine
    
    * change prop name to id
    
    * add props to header + use dynamic routing ID to query events
    
    * use dynamic routing in sidebar
    
    * add comment
    
    * formatting
    
    * changed headings for page
    
    * use new card component
    
    * Update card.tsx
    
    update to nextJS image component
    
    * change to NextJS <Image>
    
    * Put assignee badge in /volunteers
    
    * fix formatting
    
    * #issue-10-fixing-header-at-the-top-with-background-glass-effect
    #issue-10-changing-cards-per-column
    
    * adding precaution before fetching API
    
    * switched placement of status pill and badge, changed a little styling as well
    
    * badges now don't look weird if you only provide first name
    
    * changed card colors from slate to grey
    
    * improved sidebar button styling
    
    * added placeholder new card/repair request
    
    * added public images tab to event dashboard
    
    * minor quality of life changes
    
    * #issue-10-changing-code-following-the-comments
    
    * fixed typo, cards can have custom onClick functions
    
    * #issue-10-refactoring-code
    
    * Cards now pop up a Modal when clicked
    
    * Card pops up a prepopulated repair-request-form, Modal component title is now optional
    
    * fix old variable
    
    * fixing data rendering issue, arranging endpoints in single directory
    
    * removing, replacing unrequired hooks
    
    * update endpoint directory, updated add button
    
    * formatting with prettier
    
    * (WIP) Cards now have their forms prepopulated w/ data
    
    * (+) Button now pops up a Modal to create a new repair request
    
    * fix type for default values
    
    * remove the field for id from the prepopulated form
    
    * removing poop from globals.css to fix event-dashboard
    
    * changed name of repair request page, remove console.log
    
    * fix casing + remove inter declaration
    
    * remove inter declaration
    
    * fix UI vs ui directory conflict by creating Search/
    
    ---------
    
    Co-authored-by: Juan <juan.selvan77@gmail.com>
    Co-authored-by: Dylan To <19729435+dct0@users.noreply.github.com>
    Co-authored-by: Juan Selvan <122077337+juan-selvan@users.noreply.github.com>
    Co-authored-by: Stanley Natalius <22915411@student.uwa.edu.au>
    Co-authored-by: bobojeager <danishhilmann98@gmail.com>

--- src/components/Cards/card.tsx
+++ src/components/Cards/card.tsx
@@ -1,7 +1,13 @@
-import AssigneeBade from "@/components/Cards/assignee-badge";
-import StatusPill from "@/components/Cards/status-pill";
+import { useState } from "react";
+import Image from "next/image";
+import { RepairRequest } from "@prisma/client";
 
-type Props = {
+import AssigneeBadge from "@/components/Cards/assignee-badge";
+import StatusPill from "@/components/Cards/status-pill";
+import PrepopulatedRepairAttemptForm from "@/components/Forms/prepopulated-repair-request-form";
+import Modal from "@/components/Modal/index";
+
+export type CardProps = {
   title?: string;
   description?: string;
   image?: string;
@@ -9,51 +15,68 @@ type Props = {
   firstName?: string;
   lastName?: string;
   avatar?: string;
+  handleClick?: () => void;
+  repairRequestProps: RepairRequest;
 };
 
-export default function Card({
-  title,
-  description,
-  image,
-  status,
-  firstName,
-  lastName,
-  avatar
-}: Props) {
+export default function Card({ props }: { props: CardProps }) {
   function handleClick() {
-    alert("Something happens");
+    setShowModal(true);
   }
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div
-      onClick={handleClick}
-      onKeyDown={handleClick}
+      onClick={props.handleClick ? props.handleClick : handleClick}
+      onKeyDown={props.handleClick ? props.handleClick : handleClick}
       role="presentation"
-      className="col-span-1 w-72 flex-col overflow-hidden rounded-lg bg-slate-200 shadow-2xl transition hover:-translate-y-0.5 hover:cursor-pointer hover:bg-slate-100"
+      className="group col-span-1 max-w-xs flex-col overflow-hidden rounded-lg bg-grey-100 shadow-md transition hover:-translate-y-0.5 hover:cursor-pointer hover:bg-grey-50"
     >
-      <div className="flex justify-center">
-        <img src={image} alt="" className="object-fit max-h-32 w-full" />
-      </div>
-      <div className="p-2">
-        <div className="mb-1 flex flex-row items-start justify-between">
-          <h3 className="h-14 w-1/2 self-start overflow-hidden text-ellipsis text-xl font-bold">
-            {title}
-          </h3>
-          <div className="">
-            <AssigneeBade
-              firstName={firstName}
-              lastName={lastName}
-              avatar={avatar}
-            />
+      <Modal setShowPopup={setShowModal} showModal={showModal} height="h-full">
+        <div className="text-center">
+          <h1 className="text-xl font-bold">Repair ID:</h1>
+          <h2 className="text-l font-bold">
+            {props.title} <StatusPill status={props.status} />
+          </h2>
+          <div>
+            <PrepopulatedRepairAttemptForm
+              props={props.repairRequestProps}
+            ></PrepopulatedRepairAttemptForm>
           </div>
         </div>
+      </Modal>
+      <div className="flex justify-center">
+        <Image
+          src={props.image || ""}
+          alt="hello"
+          className="object-fit max-h-32 w-full"
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className="p-2">
+        <div className="flex flex-row items-start justify-between">
+          <h3 className="h-18 flex w-full flex-col  self-start text-xl font-bold">
+            Repair ID:
+          </h3>
+
+          <div className="pr-2">
+            <StatusPill status={props.status} />
+          </div>
+        </div>
+        <p className="mb-3 w-full text-sm font-semibold">{props.title}</p>
         <div>
           <p className="mb-3 h-32 overflow-y-scroll text-ellipsis text-sm font-light">
-            {description}
+            {props.description}
           </p>
         </div>
         <div className="m-2 mt-0 flex justify-end">
-          <StatusPill status={status} />
+          <AssigneeBadge
+            firstName={props.firstName}
+            lastName={props.lastName}
+            avatar={props.avatar}
+          />
         </div>
       </div>
     </div>
