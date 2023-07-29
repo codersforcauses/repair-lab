a26725ace95ab2cf4b0315c257a0f382969190f6
Justin Liang <justin4426@outlook.com>
2023-07-29 11:58:41 +0800

    Refactor user service to return only necessary info

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,14 +3,22 @@
 import { clerkClient } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/dist/types/server/clerkClient";
 
+interface UserResponse {
+  id: string;
+  firstName: string | null;
+  lastName: string | null;
+  emailAddress: string;
+  role: string | unknown;
+}
+
 interface IUserService {
-  getAll(): Promise<Partial<User>[]>;
-  getUser(userId: string): Promise<Partial<User>>;
-  updateRole(userId: string, role: string): Promise<User>;
+  getAll(): Promise<UserResponse[]>;
+  getUser(userId: string): Promise<UserResponse>;
+  updateRole(userId: string, role: string): void;
 }
 
 class UserService implements IUserService {
-  async getAll(): Promise<Partial<User>[]> {
+  async getAll(): Promise<UserResponse[]> {
     const users = await clerkClient.users.getUserList({
       limit: 100,
       orderBy: "-created_at"
@@ -19,12 +27,12 @@ class UserService implements IUserService {
     return users.map((user) => this.toResponse(user));
   }
 
-  async getUser(userId: string): Promise<Partial<User>> {
+  async getUser(userId: string): Promise<UserResponse> {
     const user = await clerkClient.users.getUser(userId);
     return this.toResponse(user);
   }
 
-  async updateRole(userId: string, role: string): Promise<User> {
+  async updateRole(userId: string, role: string) {
     return await clerkClient.users.updateUser(userId, {
       publicMetadata: {
         role: role
@@ -32,14 +40,17 @@ class UserService implements IUserService {
     });
   }
 
-  private toResponse(user: User): Partial<User> {
+  private toResponse(user: User): UserResponse {
     const { id, firstName, lastName, emailAddresses, publicMetadata } = user;
+    const role = publicMetadata.role;
+    const emailAddress = emailAddresses[0].emailAddress;
+
     return {
       id,
       firstName,
       lastName,
-      emailAddresses,
-      publicMetadata
+      role,
+      emailAddress
     };
   }
 }
