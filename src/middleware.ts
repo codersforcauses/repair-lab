ed8a321e106525924172d9782c959fc4eaa30f36
valtyx13 <20164481@student.curtin.edu.au>
2023-11-21 23:21:50 +0800

    lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,14 @@ import { UserRole } from "@/types";
 
 export default authMiddleware({
   // url routes that don't require login
-  publicRoutes: ["/auth/login", "/auth/sso-callback", "/auth/forgot-password", "/auth/register"],
+  publicRoutes: [
+    "/auth/login",
+    "/auth/sso-callback",
+    "/auth/forgot-password",
+    "/auth/register"
+  ],
 
-  async afterAuth(auth, req, evt){
+  async afterAuth(auth, req, evt) {
     // handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
@@ -21,9 +26,13 @@ export default authMiddleware({
       const roleProtectedRoutes = buildRoleProtectedRoutes();
 
       // handle access to routes based on user role
-      if (!(roleProtectedRoutes[user.role].some(regex => regex.test(req.nextUrl.pathname)))){
-        if(!(req.nextUrl.pathname.startsWith("/api/"))){
-          return NextResponse.json({ error: 'Access Denied' }, { status: 401 })
+      if (
+        !roleProtectedRoutes[user.role].some((regex) =>
+          regex.test(req.nextUrl.pathname)
+        )
+      ) {
+        if (!req.nextUrl.pathname.startsWith("/api/")) {
+          return NextResponse.json({ error: "Access Denied" }, { status: 401 });
         }
       }
     }
@@ -31,15 +40,26 @@ export default authMiddleware({
 });
 
 // map of routes protected based on roles
-type ProtectedRouteMap = Record<UserRole, RegExp[]>
+type ProtectedRouteMap = Record<UserRole, RegExp[]>;
 
 const buildRoleProtectedRoutes = (): ProtectedRouteMap => {
   // TODO: move protection of api routes to endpoints
-  const publicRoutes = [new RegExp(PageRouteRegex.AUTH)]
-  const clientRoutes = [...publicRoutes, new RegExp(PageRouteRegex.ROOT), new RegExp(PageRouteRegex.REPAIR_REQUEST)]
-  const repairerRoutes = [...clientRoutes, new RegExp(PageRouteRegex.REPAIR_ATTEMPT)]
-  const eventManagerRoutes = [...clientRoutes, ...repairerRoutes, new RegExp(PageRouteRegex.EVENTS)]
-  const adminRoutes = [new RegExp(PageRouteRegex.ALL)]
+  const publicRoutes = [new RegExp(PageRouteRegex.AUTH)];
+  const clientRoutes = [
+    ...publicRoutes,
+    new RegExp(PageRouteRegex.ROOT),
+    new RegExp(PageRouteRegex.REPAIR_REQUEST)
+  ];
+  const repairerRoutes = [
+    ...clientRoutes,
+    new RegExp(PageRouteRegex.REPAIR_ATTEMPT)
+  ];
+  const eventManagerRoutes = [
+    ...clientRoutes,
+    ...repairerRoutes,
+    new RegExp(PageRouteRegex.EVENTS)
+  ];
+  const adminRoutes = [new RegExp(PageRouteRegex.ALL)];
   // TODO: organisation manager routes
 
   return {
@@ -48,15 +68,15 @@ const buildRoleProtectedRoutes = (): ProtectedRouteMap => {
     [UserRole.EVENT_MANAGER]: eventManagerRoutes,
     [UserRole.ADMIN]: adminRoutes,
     [UserRole.ORGANISATION_MANAGER]: eventManagerRoutes
-  }
-}
+  };
+};
 
 // middleware route
 export enum PageRouteRegex {
-  ROOT = "/\$",
+  ROOT = "/$",
   AUTH = "/auth",
   EVENTS = "/events",
-  REPAIR_REQUEST = "/repair-request\$",
+  REPAIR_REQUEST = "/repair-request$",
   REPAIR_ATTEMPT = "/repair-request",
   ALL = "/"
 }
