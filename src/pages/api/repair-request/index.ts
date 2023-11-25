708ad996dfa0a3f1d5b7977bfd602971be415a4e
James van de Ven <hello@jamesvandeven.com>
2023-11-25 14:33:05 +0800

    example get repair requests method

--- src/pages/api/repair-request/index.ts
+++ src/pages/api/repair-request/index.ts
@@ -1,13 +1,37 @@
 import type { NextApiRequest, NextApiResponse, PageConfig } from "next";
+import { ApiError } from "next/dist/server/api-utils";
 import { getAuth } from "@clerk/nextjs/server";
+import { HttpStatusCode } from "axios";
 
 import apiHandler from "@/lib/api-handler";
 import prisma from "@/lib/prisma";
 import { createRepairRequestSchema } from "@/schema/repair-request";
+import userService from "@/services/user";
+import { UserRole } from "@/types";
 
 export default apiHandler({
+  get: getRepairRequests,
   post: createRepairRequest
 });
+
+async function getRepairRequests(req: NextApiRequest, res: NextApiResponse) {
+  const { userId } = getAuth(req);
+  if (!userId)
+    throw new ApiError(HttpStatusCode.BadRequest, "User not signed in");
+  const role = await userService.getRole(userId);
+
+  const response = await prisma.repairRequest.findMany(
+    role === UserRole.ADMIN
+      ? undefined
+      : {
+          where: {
+            OR: [{ createdBy: userId }, { assignedTo: userId }]
+          }
+        }
+  );
+
+  return res.status(200).json(response);
+}
 
 async function createRepairRequest(req: NextApiRequest, res: NextApiResponse) {
   const parsedData = createRepairRequestSchema.parse(req.body);
