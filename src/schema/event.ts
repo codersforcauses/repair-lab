f5af91e87bb189eb5d607cdd63733f828d5dd9fb
johnald1337 <johnald1337@hotmail.com>
2023-12-01 20:53:47 +0800

    Prettier in events.ts

--- src/schema/event.ts
+++ src/schema/event.ts
@@ -16,61 +16,70 @@ const dateStringToISO = (
   return parsed.toISOString();
 };
 
-export const createEventSchema = z.object({
-  name: z.string().min(1, { message: "Event name is required" }),
-  location: z.string().min(1, { message: "Event location is required" }),
-  description: z.string().min(5, {
-    message: "Description of the event must be more than 5 characters long."
-  }),
-  disclaimer: z.string().optional(),
-  eventType: z.string().min(1, { message: "Event type is required" }),
-  startDate: z
-    .string()
-    .min(1, { message: "Start Date is required" })
-    .transform((str, ctx) =>
-      dateStringToISO(str, ctx, "Invalid date format for Start Date")
-    ),
-  endDate: z
-    .string()
-    .min(1, { message: "End Date is required" })
-    .transform((str, ctx) =>
-      dateStringToISO(str, ctx, "Invalid date format for End Date")
-    )
-}).refine((data) => new Date(data.endDate) >= new Date(data.startDate), {   // validation to ensure the end date comes after the start date
-    message: "End Date must be later than Start Date",
-    path: ["endDate"], 
-});
-
-export const updateEventSchema = z.object({
-  name: z.string().optional(),
-  location: z.string().optional(),
-  description: z
-    .string()
-    .min(5, {
+export const createEventSchema = z
+  .object({
+    name: z.string().min(1, { message: "Event name is required" }),
+    location: z.string().min(1, { message: "Event location is required" }),
+    description: z.string().min(5, {
       message: "Description of the event must be more than 5 characters long."
-    })
-    .optional(),
-  disclaimer: z.string().optional(),
-  eventType: z.string().optional(),
-  startDate: z
-    .string()
-    .transform((str, ctx) =>
-      dateStringToISO(str, ctx, "Invalid date format for Start Date")
-    )
-    .optional(),
-  endDate: z
-    .string()
-    .transform((str, ctx) =>
-      dateStringToISO(str, ctx, "Invalid date format for End Date")
-    )
-    .optional(),
-  status: z.enum(["UPCOMING", "ONGOING", "COMPLETED"]).optional()
-}).refine((data) => {
-  if (data.startDate && data.endDate) {                                 // validation to ensure the end date comes after the start date
-    return new Date(data.endDate) >= new Date(data.startDate);
-  }
-  return true;
-}, {
-  message: "End Date must be later than Start Date",
-  path: ["endDate"], 
-});
+    }),
+    disclaimer: z.string().optional(),
+    eventType: z.string().min(1, { message: "Event type is required" }),
+    startDate: z
+      .string()
+      .min(1, { message: "Start Date is required" })
+      .transform((str, ctx) =>
+        dateStringToISO(str, ctx, "Invalid date format for Start Date")
+      ),
+    endDate: z
+      .string()
+      .min(1, { message: "End Date is required" })
+      .transform((str, ctx) =>
+        dateStringToISO(str, ctx, "Invalid date format for End Date")
+      )
+  })
+  .refine((data) => new Date(data.endDate) >= new Date(data.startDate), {
+    // validation to ensure the end date comes after the start date
+    message: "End Date must be later than Start Date",
+    path: ["endDate"]
+  });
+
+export const updateEventSchema = z
+  .object({
+    name: z.string().optional(),
+    location: z.string().optional(),
+    description: z
+      .string()
+      .min(5, {
+        message: "Description of the event must be more than 5 characters long."
+      })
+      .optional(),
+    disclaimer: z.string().optional(),
+    eventType: z.string().optional(),
+    startDate: z
+      .string()
+      .transform((str, ctx) =>
+        dateStringToISO(str, ctx, "Invalid date format for Start Date")
+      )
+      .optional(),
+    endDate: z
+      .string()
+      .transform((str, ctx) =>
+        dateStringToISO(str, ctx, "Invalid date format for End Date")
+      )
+      .optional(),
+    status: z.enum(["UPCOMING", "ONGOING", "COMPLETED"]).optional()
+  })
+  .refine(
+    (data) => {
+      if (data.startDate && data.endDate) {
+        // validation to ensure the end date comes after the start date
+        return new Date(data.endDate) >= new Date(data.startDate);
+      }
+      return true;
+    },
+    {
+      message: "End Date must be later than Start Date",
+      path: ["endDate"]
+    }
+  );
