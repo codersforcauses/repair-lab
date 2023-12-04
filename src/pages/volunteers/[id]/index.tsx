2fcb54e7b9353e36d0091d572a9a3d7eb897aff0
Delta O <deltaoliver@gmail.com>
2023-12-04 11:14:43 +0800

    Added passing of properties
    Should be easy enough to add/ remove properties as required

--- src/pages/volunteers/[id]/index.tsx
+++ src/pages/volunteers/[id]/index.tsx
@@ -5,8 +5,8 @@ import Box from "@/components/EventBox/Box";
 
 const Home = () => {
 
-return (
-<div>
+  return (
+    <div>
       {/* HEADER BAR*/}
       <div className="relative z-10 mt-2 flex w-full justify-center">
         <Image
@@ -16,17 +16,17 @@ return (
           height={90}
         />
       </div>
-        <h1 className="relative z-10 mt-2 flex w-full justify-center">
-          My Events
-        </h1>
-         <hr/>
-         <div className="relative flex-row items-center justify-center">
-            <Box/>
-            
-            <Box/>
-         </div>
-</div>
-);
+      <h1 className="relative z-10 mt-2 flex w-full justify-center">
+        My Events
+      </h1>
+      <hr />
+      <div className="relative flex-row items-center justify-center">
+        <Box eventTitle="an event" startDate="1-January" endDate="2-January" description="wow its an event" />
+
+        <Box eventTitle="another event" startDate="3-January" endDate="4-January" description="wonder if i can link to text files" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
