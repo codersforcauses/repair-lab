0b9ac22a6d28d8439552ae0b87f40a9e5f5deb7e
lumenesce <richellechen2000@gmail.com>
2023-12-09 14:03:23 +0800

    doesn't matter what it is we're just going to undo

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -3,6 +3,7 @@ import { useUser } from "@clerk/nextjs";
 import { UserRole } from "@/types";
 
 export const useAuth = () => {
+  console.log("pls work");
   const { user, isSignedIn, isLoaded } = useUser();
 
   const role = (user?.publicMetadata.role ?? UserRole.CLIENT) as UserRole; // default to CLIENT if no roles set.
