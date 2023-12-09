38671234917054261099b4f2cff251d414a73ef4
lumenesce <richellechen2000@gmail.com>
2023-12-09 14:04:47 +0800

    undid changes :)

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -3,7 +3,6 @@ import { useUser } from "@clerk/nextjs";
 import { UserRole } from "@/types";
 
 export const useAuth = () => {
-  console.log("pls work");
   const { user, isSignedIn, isLoaded } = useUser();
 
   const role = (user?.publicMetadata.role ?? UserRole.CLIENT) as UserRole; // default to CLIENT if no roles set.
