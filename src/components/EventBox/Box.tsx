dae224b22d92ce6d95ee7998384a25eed593e13e
Delta O <deltaoliver@gmail.com>
2023-12-11 11:23:56 +0800

    Changed into a headless-ui closure, animation working if arbitrary
    Still an arbitrary max height tho, cant be otherwise afaik

--- src/components/EventBox/Box.tsx
+++ src/components/EventBox/Box.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Disclosure, Transition } from '@headlessui/react';
+
 
 // Contains type of info stored in our event box.
 type BoxProps = {
@@ -32,12 +34,12 @@ const Box = ({
 
   return (
     <div className="mx-5 mt-4 rounded-lg bg-slate-200 shadow-lg">
-      <div className={`flex justify-between flex-row text-lg rounded-lg transition-colors duration-300 ease-in-out ${!expanded ? "bg-slate-200" : "bg-slate-300"} `}
-        role='button'
-        tabIndex={0}
-        onKeyDown={handleClick}
-        onClick={handleClick}>
-        {/* <span className="flex-none">
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button as="div" className="flex justify-between flex-row text-lg rounded-lg ">
+              {/* transition-colors duration-300 ease-in-out ${!expanded ? "bg-slate-200" : "bg-slate-300"}*/}
+              {/* <span className="flex-none">
           <Image
             src={imagePath}
             alt="event image"
@@ -46,43 +48,55 @@ const Box = ({
             className="w-20 h-20 ml-1 rounded-full scale-95"
           />
         </span> */}
-        <div className="flex flex-col ml-2 mr-2">
-          <span className="font-bold pt-2">
-            {eventTitle}
-          </span>
-          <div className="pt-1 italic border-spacing-2">
-            {startDate} - {endDate}
-          </div>
-        </div>
+              <div className="flex flex-col ml-2 mr-2">
+                <span className="font-bold pt-2">
+                  {eventTitle}
+                </span>
+                <div className="pt-1 italic border-spacing-2">
+                  {startDate} - {endDate}
+                </div>
+              </div>
 
-        <div className="justify-right mr-2 -mt-1">
-          <svg fill="None" viewBox="0 0 30 30" strokeWidth={2}
-            stroke="black" className="flex w-7 h-7 mt-8 justify-right">;
-            <path
-              // Used icons from https://heroicons.com/
-              // Open source, MIT license. 
-              d={expanded ? "M4.5 15.75l7.5-7.5 7.5 7.5"
-                : "M19.5 8.25l-7.5 7.5-7.5-7.5"
-              }
-            />
-          </svg>
-        </div>
-      </div>
+              <div className="justify-right mr-2 -mt-1">
+                <svg fill="None" viewBox="0 0 30 30" strokeWidth={2}
+                  stroke="black" className="flex w-7 h-7 mt-8 justify-right">;
+                  <path
+                    // Used icons from https://heroicons.com/
+                    // Open source, MIT license. 
+                    d={open ? "M4.5 15.75l7.5-7.5 7.5 7.5"
+                      : "M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    }
+                  />
+                </svg>
+              </div>
+            </Disclosure.Button>
 
-      <div className={`pt-1 relative mb-2 mx-2 overflow-hidden 
-      transition-[max-height] duration-500 ease-in-out ${expanded ? "max-h-96" : "max-h-0"}`}>
-        {description}
-          
-        <div role='button'
-            tabIndex={0}
-            className="bg-primary-800 px-2000 opacity-50 rounded-lg text-white h-6 text-md hover:bg-primary-800 text-center"
-        >
-          See Repair Requests {">"}
-        </div>
+            <Transition
+              className="overflow-hidden"
+              enter="transition-all ease-in-out duration-500"
+              enterFrom="transform  max-h-0"
+              enterTo="transform  max-h-[800px]"
+              leave="transition-all ease-in-out duration-500"
+              leaveFrom="transform  max-h-[800px]"
+              leaveTo="transform  max-h-0"
+            >
+              <Disclosure.Panel className="pt-1 relative mb-2 mx-2">
+                {description}
 
-      </div>
+                <div role='button'
+                  tabIndex={0}
+                  className="bg-primary-800 px-2000 opacity-50 rounded-lg text-white h-6 text-md hover:bg-primary-800 text-center"
+                >
+                  See Repair Requests {">"}
+                </div>
 
-    
+              </Disclosure.Panel>
+
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+
     </div>
   );
 };
