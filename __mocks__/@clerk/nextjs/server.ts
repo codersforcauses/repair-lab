d02cc57c7f72adb6a3a580d50600eb9419a1091a
alexbarker234 <alex.barker234@gmail.com>
2023-12-12 08:32:08 +0800

    removed commented mock lines

--- __mocks__/@clerk/nextjs/server.ts
+++ __mocks__/@clerk/nextjs/server.ts
@@ -1,16 +1,1 @@
 export const getAuth = () => ({ userId: "Test" });
-
-// vi.mock("@clerk/nextjs/server", () => {
-//   return {
-//     getAuth: vi.fn().mockReturnValue({ userId: "Test" })
-//   };
-// });
-// vi.mock("@clerk/nextjs", () => {
-//   return {
-//     clerkClient: {
-//       users: {
-//         getUserList: vi.fn().mockReturnValue(mockClerkUsers)
-//       }
-//     }
-//   };
-// });
