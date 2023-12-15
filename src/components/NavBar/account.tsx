36d068508c5e3febda8d732847db6cc5871abcee
johnald1337 <johnald1337@hotmail.com>
2023-12-15 12:41:21 +0800

    copy basic new event button

--- src/components/NavBar/account.tsx
+++ src/components/NavBar/account.tsx
@@ -23,14 +23,21 @@ export default function Account({
   const router = useRouter();
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center ">
       {isLoggedIn ? (
         <>
           <button
             onClick={() => router.push("/repair-request")}
-            className="flex items-center justify-center px-2 placeholder:w-[160px] h-[40px] rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
+            className="flex items-center justify-center px-2 placeholder:w-[160px] h-[40px]  rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
           >
             New Repair Request +
+          </button>
+
+          <button
+            onClick={() => router.push("/repair-request")}
+            className="flex items-center justify-center px-2 placeholder:w-[160px] h-[40px] rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
+          >
+            New Event +
           </button>
 
           <ActionButton onClick={onSignOut} label="Log Out" />
