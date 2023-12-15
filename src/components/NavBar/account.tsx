3e24b09e19a1937b3b98f5e9bdd9cabac15a79ad
johnald1337 <johnald1337@hotmail.com>
2023-12-15 13:46:05 +0800

    margin to + buttons

--- src/components/NavBar/account.tsx
+++ src/components/NavBar/account.tsx
@@ -23,19 +23,19 @@ export default function Account({
   const router = useRouter();
 
   return (
-    <div className="flex items-center ">
+    <div className="flex items-center">
       {isLoggedIn ? (
         <>
           <button
             onClick={() => router.push("/repair-request")}
-            className="flex items-center justify-center px-2 placeholder:w-[160px] h-[40px]  rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
+            className="flex items-center justify-center px-2 mx-4 placeholder:w-[160px] h-[40px]  rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
           >
             New Repair Request +
           </button>
 
           <button
             onClick={() => router.push("/events?openModal=true")}
-            className="flex items-center justify-center px-2 placeholder:w-[160px] h-[40px] rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
+            className="flex items-center justify-center px-2 mx-4 placeholder:w-[160px] h-[40px] rounded-full bg-primary-700 text-white font-medium outline-none hover:bg-primary-800"
           >
             New Event +
           </button>
