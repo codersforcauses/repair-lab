6e1a2a86d184053cfdcc5f89638c5636fe31452c
tommyjqliu <tommyjqliu@outlook.com>
2023-12-16 14:37:21 +0800

    chore: extract env

--- src/lib/aws-client.ts
+++ src/lib/aws-client.ts
@@ -1,11 +1,8 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-export const AWS_BUCKET_NAME = "repair-lab-images"; // TODO: extract to env
+export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME!;
+export const AWS_REGION = process.env.AWS_REGION!;
 
 export const s3client = new S3Client({
-  region: "ap-southeast-2",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!
-  }
+  region: AWS_REGION
 });
