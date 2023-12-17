c9dd64373426cef7e9415f290d9ec95b9255ce28
johnald1337 <johnald1337@hotmail.com>
2023-12-17 13:09:09 +0800

    current page underlined in navbar

--- src/components/NavBar/menu-list.tsx
+++ src/components/NavBar/menu-list.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Tab } from "@headlessui/react";
 
 import { NavPath } from "@/types";
@@ -7,14 +8,18 @@ interface Props {
   items: Array<NavPath>;
 }
 
-export default function MenuList({ items }: Props) {
+export default function MenuList({ items }: Readonly<Props>) {
+  const router = useRouter();
+
   return (
     <Tab.Group>
       <Tab.List className="flex pl-1">
         {items.map((item) => (
           <Tab
             key={item.item}
-            className="w-[160px] h-[60px] rounded-lg font-medium outline-none text-black hover:bg-slate-100 hover:text-primary-700"
+            className={`w-[160px] h-[60px] rounded-lg font-medium outline-none text-black hover:bg-slate-100 hover:text-primary-700 ${
+              router.asPath === item.path ? "underline text-primary-700" : ""
+            }`}
           >
             <Link href={item.path}>
               <div className="flex items-center justify-center h-full w-full">
