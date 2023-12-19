6201b914f4426b2700a647b7eedf6088354cbcc3
Delta O <deltaoliver@gmail.com>
2023-12-19 10:26:58 +0800

    Formatting of tabs fixed

--- src/pages/my-events/[id].tsx
+++ src/pages/my-events/[id].tsx
@@ -12,22 +12,22 @@ import { useRepairRequests } from "@/hooks/events";
 const Home = () => {
 
   const {
-    query: {id: eventId}
+    query: { id: eventId }
   } = useRouter();
 
   console.log(eventId)
 
-  const {isLoading, data: repairRequests} = useRepairRequests(eventId as string);
-  const {data: event} = useEvent(eventId as string);
+  const { isLoading, data: repairRequests } = useRepairRequests(eventId as string);
+  const { data: event } = useEvent(eventId as string);
 
   return (
 
-   <div>  
+    <div>
 
 
-    
-    {/* HEADER BAR*/}
-          <div className="relative z-10 mt-2 flex w-full justify-center">
+
+      {/* HEADER BAR*/}
+      <div className="relative z-10 mt-2 flex w-full justify-center">
         <Image
           src="/images/repair_lab_logo.png"
           alt="Repair Labs Logo"
@@ -35,55 +35,53 @@ const Home = () => {
           height={90}
         />
       </div>
-      
+
       <h1 className="relative z-10 mt-2 text-xl flex w-full justify-center">
-        {event ? event.name : <LoadingSpinner/>}
+        {event ? event.name : <LoadingSpinner />}
       </h1>
       <hr className="mx-10" />
 
       <div className="mt-4 relative flex-row items-center justify-center text-lg mx-10 bg-slate-200 rounded-lg">
 
         <Tab.Group>
-          <Tab.List className="flex justify-around rounded-t-lg bg-slate-300 rounded-lg">
+          <Tab.List className="flex justify-center rounded-t-lg bg-slate-300 rounded-lg">
 
             <Tab as={Fragment}>
               {({ selected }) => (
-            <button
-              className={selected ? 'bg-slate-200 ' : 'bg-slate-300'
-              }
-            >
-              My Repairs
-            </button>
-          )}
-          </Tab>
+                <button
+                  className={`rounded-tl-lg grow ${selected ? 'bg-slate-200 ' : 'bg-slate-300'}`}
+                >
+                  My Repairs
+                </button>
+              )}
+            </Tab>
 
-          <Tab as={Fragment}>
+            <Tab as={Fragment}>
               {({ selected }) => (
-            <button
-              className={selected ? 'bg-slate-200 ' : 'bg-slate-300 '
-              }
-            >
-              Available
-            </button>
-          )}
-          </Tab>
+                <button
+                  className={`rounded-tr-lg grow ${selected ? 'bg-slate-200 ' : 'bg-slate-300'}`}
+                >
+                  Available
+                </button>
+              )}
+            </Tab>
 
           </Tab.List>
 
 
-            <Tab.Panels className="flex-col text-center break-words justify-center">
-           
+          <Tab.Panels className="flex-col text-center break-words justify-center">
+
             <Tab.Panel><div className=""> Content1 </div></Tab.Panel>
             <Tab.Panel><div className="">Content 2</div></Tab.Panel>
 
-    
+
 
           </Tab.Panels>
         </Tab.Group>
 
       </div>
-  
-  </div>     
+
+    </div >
   )
 }
 
