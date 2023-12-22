92e6f411089837b2b18293bfb6939da76ea89ce0
alexbarker234 <alex.barker234@gmail.com>
2023-12-22 16:20:48 +0800

    ability to add classnames directly on loader component

--- src/components/UI/loading-spinner.tsx
+++ src/components/UI/loading-spinner.tsx
@@ -1,22 +1,24 @@
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ className }: { className?: string }) => {
   return (
-    <div className="flex justify-center relative w-12 h-12 text-sm">
-      <div className="absolute top-[0] left-[0] transform w-1 h-1">
-        <FontAwesomeIcon
-          icon={faGear}
-          className="animate-spin text-primary-500 text-[1.5rem]"
-        />
+    <div className={className}>
+      <div className="flex justify-center relative w-12 h-12 text-sm">
+        <div className="absolute top-[0] left-[0] transform w-1 h-1">
+          <FontAwesomeIcon
+            icon={faGear}
+            className="animate-spin text-primary-500 text-[1.5rem]"
+          />
+        </div>
+        <div className="absolute top-[15px] left-[15px] transform w-1 h-1 bol">
+          <FontAwesomeIcon
+            icon={faGear}
+            className="animate-reverse-spin text-primary-500 text-[2rem]"
+          />
+        </div>
+        <span className="sr-only">Loading...</span>
       </div>
-      <div className="absolute top-[15px] left-[15px] transform w-1 h-1 bol">
-        <FontAwesomeIcon
-          icon={faGear}
-          className="animate-reverse-spin text-primary-500 text-[2rem]"
-        />
-      </div>
-      <span className="sr-only">Loading...</span>
     </div>
   );
 };
