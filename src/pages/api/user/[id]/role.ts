c82d36bbea9cc4638c58148e5e676957a86f5aa1
Justin Liang <justin4426@outlook.com>
2023-12-26 10:34:45 +0800

    Create a wrapper for getAuth from clerk to user service

--- src/pages/api/user/[id]/role.ts
+++ src/pages/api/user/[id]/role.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ApiError } from "next/dist/server/api-utils";
-import { getAuth } from "@clerk/nextjs/server";
 import { HttpStatusCode } from "axios";
 import { z } from "zod";
 
@@ -19,9 +18,8 @@ async function updateUserRole(req: NextApiRequest, res: NextApiResponse) {
   const parsedRole = validateRole(role);
 
   // only admins can update a user's role
-  const { userId: updaterId } = getAuth(req);
-  // TODO: move this to a more generic function to check permissions.
-  const updaterRole = await userService.getRole(updaterId!);
+  const { role: updaterRole } = await userService.getAuth(req);
+
   if (updaterRole !== UserRole.ADMIN) {
     throw new ApiError(
       HttpStatusCode.Unauthorized,
