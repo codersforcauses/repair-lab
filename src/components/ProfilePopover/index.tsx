b1adb337d34890816fb526c7c8f65d605135e8db
ellenxiao0412@gmail.com <ellenxiao0412@gmail.com>
2024-01-06 12:25:37 +0800

    bugfix to profile popover

--- src/components/ProfilePopover/index.tsx
+++ src/components/ProfilePopover/index.tsx
@@ -20,7 +20,7 @@ export default function ProfilePopover() {
   const [initialDescription, setInitialDescription] = useState(description);
 
   useEffect(() => {
-    setDescription(String(user?.unsafeMetadata.description) || "");
+    setDescription((user?.unsafeMetadata.description as string) || "");
   }, [user?.unsafeMetadata.description]);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -83,7 +83,7 @@ export default function ProfilePopover() {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <Popover.Panel className="absolute top-14 -right-5 w-80 h-96 rounded-lg bg-white z-10 shadow-custom">
+        <Popover.Panel className="absolute top-[62px] right-5 w-80 h-96 rounded-lg bg-white z-10 shadow-custom">
           <div className="flex flex-col items-center">
             <div className="relative w-28 h-28 rounded-full my-6">
               {user?.imageUrl && (
