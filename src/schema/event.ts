f5edad5ba019262c3e03ee59aed84329633be377
Feifan <wolfgang1208@outlook.com>
2024-01-13 11:08:01 +0800

    fix some of the string parameters

--- src/schema/event.ts
+++ src/schema/event.ts
@@ -3,23 +3,25 @@ import { z } from "zod";
 export const createEventSchema = z
   .object({
     name: z
-      .string()
+      .string({ required_error: "Event name is required" })
       .min(1, { message: "Event name must be 1 or more characters long" }),
     location: z
-      .string()
+      .string({ required_error: "Event location is required" })
       .min(1, { message: "Event location must be 1 or more characters long" }),
-    description: z.string().min(5, {
-      message: "Description of the event must be more than 5 characters long."
-    }),
+    description: z
+      .string({ required_error: "Description of the event is required" })
+      .min(5, {
+        message: "Description of the event must be more than 5 characters long."
+      }),
     disclaimer: z.string().optional(),
     eventType: z
-      .string()
+      .string({ required_error: "Event type is required" })
       .min(1, { message: "Event type must be 1 or more characters long" }),
     startDate: z
-      .string()
+      .string({ required_error: "startDate is required" })
       .datetime({ offset: true, message: "Invalid date format for startDate" }),
     endDate: z
-      .string()
+      .string({ required_error: "endDate is required" })
       .datetime({ offset: true, message: "Invalid date format for endDate" })
   })
   .refine((data) => new Date(data.endDate) >= new Date(data.startDate), {
@@ -30,10 +32,10 @@ export const createEventSchema = z
 
 export const addEventRepairerSchema = z.object({
   userId: z
-    .string()
+    .string({ required_error: "UserId is required" })
     .min(1, { message: "UserId must be 1 or more characters long" }),
   id: z
-    .string()
+    .string({ required_error: "Event ID is required" })
     .min(1, { message: "Event ID must be 1 or more characters long" })
 });
 
@@ -42,7 +44,7 @@ export const updateEventSchema = z
     name: z.string().optional(),
     location: z.string().optional(),
     description: z
-      .string()
+      .string({ required_error: "Description of the event is required" })
       .min(5, {
         message: "Description of the event must be more than 5 characters long."
       })
