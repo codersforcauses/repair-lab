b53ab94605d1b841bf82a99394e0f6d867320261
alexbarker234 <alex.barker234@gmail.com>
2024-01-13 14:19:09 +0800

    switched to react-icons

--- src/components/UI/loading-spinner.tsx
+++ src/components/UI/loading-spinner.tsx
@@ -1,21 +1,14 @@
-import { faGear } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaGear } from "react-icons/fa6";
 
 const LoadingSpinner = ({ className }: { className?: string }) => {
   return (
     <div className={className}>
       <div className="relative w-12 h-12 text-sm">
         <div className="absolute w-1 h-1">
-          <FontAwesomeIcon
-            icon={faGear}
-            className="animate-spin text-primary-500 text-[1.5rem]"
-          />
+          <FaGear className="animate-spin text-primary-500 text-[1.5rem]" />
         </div>
         <div className="absolute top-[15px] left-[15px] transform w-1 h-1">
-          <FontAwesomeIcon
-            icon={faGear}
-            className="animate-reverse-spin text-primary-500 text-[2rem]"
-          />
+          <FaGear className="animate-reverse-spin text-primary-500 text-[2rem]" />
         </div>
         <span className="sr-only">Loading...</span>
       </div>
