269c9791f54e804011c7636d7d11da7bb008c2c5
Tommy Liu <tommyjqliu@outlook.com>
2024-01-16 12:40:20 +0800

    chore: adjust file structure

--- src/components/no-ssr.tsx
+++ src/components/no-ssr.tsx
@@ -4,7 +4,9 @@ import dynamic from "next/dynamic";
 const NoSsr = (props: { children: ReactNode }) => (
   <Fragment>{props.children}</Fragment>
 );
-
+/**
+ * Stop next.js pre-rendering component, mostly used for debugging during dev
+ */
 export default dynamic(() => Promise.resolve(NoSsr), {
   ssr: false
 });
