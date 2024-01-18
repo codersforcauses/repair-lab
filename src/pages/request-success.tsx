435b277d3d230db169f30353894e6529f8d6b2b1
chloetychang <chloetychang@gmail.com>
2024-01-19 04:10:19 +0800

    Basic layout - no formatting whatsoever

--- src/pages/request-success.tsx
+++ src/pages/request-success.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import type { ReactElement } from "react";
 import Link from "next/link";
-import { useClerk } from "@clerk/nextjs";
-import { AiFillFacebook, AiFillInstagram, AiFillMail } from "react-icons/ai";
 import type { NextPageWithLayout } from "./_app";
 import Button from "@/components/Button/index";
 import NavBar from "@/components/NavBar";
@@ -10,23 +8,27 @@ import NavBar from "@/components/NavBar";
 // #TODO: Refactor this
 const Success: NextPageWithLayout = () => {
   return (
-    <div className="relative h-44 w-full">
-      <Image
-        src="/images/sewing_box.jpg"
-        alt="box of sewing equipment"
-        fill={true}
-        className="object-cover object-center"
-      />
+    <div className="relative">
+      <div className="mb-8 mt-6 text-center text-3xl font-bold text-grey-900">
+        Thank you!
+      </div>
+
+      <Link href="/repair-request">
+        <Button width="w-1/4"> Submit another request </Button>
+      </Link>
+      <Link href="/index">
+        <Button width="w-1/4"> Return to Homepage </Button>
+      </Link>
     </div>
   );
+};
 
-  Success.getLayout = function getLayout(page: ReactElement) {
-    return (
-      <>
-        <NavBar />
-        {page}
-      </>
-    );
-  };
+Success.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <>
+      <NavBar />
+      {page}
+    </>
+  );
 };
 export default Success;
