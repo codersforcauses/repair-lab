b5a82c954dba4799cf77fbe8941e1e5d1230d8e0
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-01-20 14:51:35 +0800

    Ensuring event boxes have equal height

--- src/components/Cards/event-card.tsx
+++ src/components/Cards/event-card.tsx
@@ -8,23 +8,27 @@ export type CardProps = {
 export default function Card({props}: { props: CardProps }) {
 
   return (
-    <div>
-        <div className="group col-span-1 max-w-xs flex-col overflow-hidden rounded-lg bg-grey-100 shadow-md transition hover:-translate-y-0.5 hover:cursor-pointer hover:bg-grey-50">
+    <div className="h-full group col-span-1 max-w-xs flex-col overflow-hidden rounded-lg bg-grey-100 shadow-md transition hover:-translate-y-0.5 hover:cursor-pointer hover:bg-grey-50">
+        
+        <div >
           <Image
             src="/images/event_details.jpg"
             alt="Image Not Found"
             className="object-fit max-h-32 w-full"
             width={100}
             height={100}
-          />
-          <div className="text-center">
-              <h1 className="text-xl font-bold">{props.title}</h1>
-              <div>
-              {props.description}
-              </div>
-          </div>
-      </div>
+          /> 
+        </div>
+
+        <div className="text-center">
+            <h1 className="text-xl font-bold">{props.title}</h1>
+            <div>
+            {props.description}
+            </div>
+        </div>
+
     </div>
+
 
   )
 
