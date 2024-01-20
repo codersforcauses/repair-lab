5141758e301cb34f9f87f0c39061edf147c0e6b0
chloetychang <chloetychang@gmail.com>
2024-01-20 11:57:52 +0800

    the duck is a placeholder and spacing needs fixing

--- src/pages/request-success.tsx
+++ src/pages/request-success.tsx
@@ -8,17 +8,39 @@ import NavBar from "@/components/NavBar";
 // #TODO: Refactor this
 const Success: NextPageWithLayout = () => {
   return (
-    <div className="relative">
-      <div className="mb-8 mt-6 text-center text-3xl font-bold text-grey-900">
-        Thank you!
+    <div className="relative flex flex-row place-content-center gap-x-12">
+      <div>
+        <Image src="/images/duck.png" alt="Duck" width={400} height={400} />
       </div>
 
-      <Link href="/repair-request">
-        <Button width="w-1/4"> Submit another request </Button>
-      </Link>
-      <Link href="/index">
-        <Button width="w-1/4"> Return to Homepage </Button>
-      </Link>
+      <div>
+        <div className="relative flex text-center mb-3 mt-6 text-3xl font-bold text-grey-900">
+          Thank you!
+        </div>
+
+        <ul className="mb-3 gap-y-4">
+          <li>
+            We have received your repair request, and you should receive a
+            confirmation email shortly.
+          </li>
+
+          <li>
+            We are currently experiencing high volumes of requests, and will aim
+            to get back to you within 3-5 business days.
+          </li>
+
+          <li>We appreciate your patience! In the meantime...</li>
+        </ul>
+
+        <div className="relative flex text-center gap-x-8">
+          <Link href="/repair-request">
+            <Button radius="rounded-3xl"> Submit another request </Button>
+          </Link>
+          <Link href="/index">
+            <Button radius="rounded-3xl"> Return to Homepage </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
@@ -31,4 +53,5 @@ Success.getLayout = function getLayout(page: ReactElement) {
     </>
   );
 };
+
 export default Success;
