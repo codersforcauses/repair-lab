db7d93d6fc3b11b5ea04df9469b72578e1abc061
Kyle Leung <lkkc0300@gmail.com>
2024-01-20 15:20:04 +0800

    Animated effect for vertical nav bar popup

--- src/components/NavBar/VerticalBar.tsx
+++ src/components/NavBar/VerticalBar.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Transition } from "@headlessui/react";
 import { VscChromeClose, VscMenu } from "react-icons/vsc";
 
 import { MenuItems } from "@/components/NavBar";
@@ -44,12 +45,21 @@ const VerticalBar = (props: NavItems) => {
 
   return (
     <div className="relative">
-      <button onClick={toggleSidebar} >
+      <button onClick={toggleSidebar}>
         {isOpen ? <VscChromeClose size="28" /> : <VscMenu size="28" />}
       </button>
-      {isOpen && (
-        <div className="fixed top-[60px] z-9  inset-0 w-screen h-screen bg-white">
-          {/* <div className="flex justify-between items-center p-2 border-b">
+      <Transition
+        show={isOpen}
+        enter="transition-opacity duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        {isOpen && (
+          <div className="fixed top-[60px] z-9  inset-0 w-screen h-screen bg-white">
+            {/* <div className="flex justify-between items-center p-2 border-b">
             <Image
               src="/images/repair_lab_logo.png"
               alt="Repair Labs Logo"
@@ -60,48 +70,49 @@ const VerticalBar = (props: NavItems) => {
               <VscChromeClose size="40" />
             </button>
           </div> */}
-          {props.menuItems.map((item) => (
-            <Link href={item.path} key={item.item}>
-              <p
-                className={`block p-2 hover:-translate-x-1 hover:bg-app-base-100  duration-500 hover:opacity-100 ${
-                  router.asPath === item.path ? "opacity-100" : "opacity-40"
+            {props.menuItems.map((item) => (
+              <Link href={item.path} key={item.item}>
+                <p
+                  className={`block p-2 hover:-translate-x-1 hover:bg-app-base-100  duration-500 hover:opacity-100 ${
+                    router.asPath === item.path ? "opacity-100" : "opacity-40"
+                  }`}
+                >
+                  {item.item}
+                </p>
+              </Link>
+            ))}
+            {props.isLoggedIn && (
+              <Link
+                href="/repair-request"
+                className={`block p-2 hover:-translate-x-1 hover:bg-app-base-100 text-app-primary duration-500 hover:opacity-100 ${
+                  router.asPath === "/repair-request"
+                    ? "opacity-100"
+                    : "opacity-40"
                 }`}
               >
-                {item.item}
-              </p>
-            </Link>
-          ))}
-          {props.isLoggedIn && (
-            <Link
-              href="/repair-request"
-              className={`block p-2 hover:-translate-x-1 hover:bg-app-base-100 text-app-primary duration-500 hover:opacity-100 ${
-                router.asPath === "/repair-request"
-                  ? "opacity-100"
-                  : "opacity-40"
-              }`}
-            >
-              New Repair Request +
-            </Link>
-          )}
-          <div className="flex bottom-4 w-full">
-            {props.isLoggedIn ? (
-              <button
-                onClick={props.onSignOut}
-                className="w-full p-2 text-left hover:-translate-x-1 hover:bg-app-base-100  duration-500 opacity-40 hover:opacity-100"
-              >
-                Log Out
-              </button>
-            ) : (
-              <button
-                onClick={() => router.push("/login")}
-                className="w-full p-2 text-left hover:-translate-x-1 hover:bg-app-base-100  duration-500 opacity-40 hover:opacity-100"
-              >
-                Sign In
-              </button>
+                New Repair Request +
+              </Link>
             )}
+            <div className="flex bottom-4 w-full">
+              {props.isLoggedIn ? (
+                <button
+                  onClick={props.onSignOut}
+                  className="w-full p-2 text-left hover:-translate-x-1 hover:bg-app-base-100  duration-500 opacity-40 hover:opacity-100"
+                >
+                  Log Out
+                </button>
+              ) : (
+                <button
+                  onClick={() => router.push("/login")}
+                  className="w-full p-2 text-left hover:-translate-x-1 hover:bg-app-base-100  duration-500 opacity-40 hover:opacity-100"
+                >
+                  Sign In
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </Transition>
     </div>
   );
 };
