4713503ec16e8b0f431d4e9a750ee86041432f99
lumenesce <richellechen2000@gmail.com>
2024-01-20 15:06:11 +0800

    changed div positioning

--- src/pages/request-success.tsx
+++ src/pages/request-success.tsx
@@ -10,30 +10,23 @@ import type { NextPageWithLayout } from "./_app";
 // #TODO: Refactor this
 const Success: NextPageWithLayout = () => {
   return (
-    <div className="min-h-screen bg-lightAqua-100">
-      <div className="relative flex flex-row place-content-center gap-x-12">
+    <div className="min-h-screen bg-lightAqua-100 flex justify-center">
+      <div className="relative flex flex-row h-screen items-center justify-center gap-x-12">
         <div>
           <Image src="/images/duck.png" alt="Duck" width={400} height={400} />
         </div>
 
-        <div>
+        <div className="w-1/2">
           <div className="relative flex text-center mb-3 mt-6 text-3xl font-bold text-grey-900">
             Thank you!
           </div>
 
-          <ul className="mb-3 gap-y-4">
-            <li>
-              We have received your repair request, and you should receive a
-              confirmation email shortly.
-            </li>
-
-            <li>
-              We are currently experiencing high volumes of requests, and will
-              aim to get back to you within 3-5 business days.
-            </li>
-
-            <li>We appreciate your patience! In the meantime...</li>
-          </ul>
+          <div className="text-slate-500 text-xl mb-3 gap-y-4">
+            We have received your repair request, and you should receive a
+            confirmation email shortly. We are currently experiencing high
+            volumes of requests, and will aim to get back to you within 3-5
+            business days. We appreciate your patience! In the meantime...
+          </div>
 
           <div className="relative flex text-center gap-x-8">
             <Link href="/repair-request">
