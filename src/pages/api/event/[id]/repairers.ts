6a1d02d0bfcf8f9cf5e42e781f32313315d8910c
tong <bluedragon00000@gmail.com>
2024-01-24 16:50:36 +0800

    1. changed the return result if a clerk user is invalid and filtered out the invalid ones; 2. combined the same endpoint into one file, including both api and test.;

--- src/pages/api/event/[id]/repairers.ts
+++ src/pages/api/event/[id]/repairers.ts
@@ -1,14 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ApiError } from "next/dist/server/api-utils";
 import { HttpStatusCode } from "axios";
+import { z } from "zod";
 
 import apiHandler from "@/lib/api-handler";
 import { addEventRepairerSchema } from "@/schema/event";
 import userService from "@/services/user";
+import { User } from "@/types";
 
 import prisma from "../../../../lib/prisma";
 
 export default apiHandler({
+  get: getRepairers,
   post: createRepairer
 });
 
@@ -54,4 +57,48 @@ async function createRepairer(
   res
     .status(200)
     .json({ message: "Successfully added volunteers to an event" });
+}
+
+async function getRepairers(req: NextApiRequest, res: NextApiResponse) {
+  const eventId = z.string().parse(req.query.id);
+  const result = await getEventRepairers(eventId as string);
+
+  res.status(200).json(result);
+}
+
+async function getEventRepairers(id: string) {
+  // get repairers from table EventRepairer
+  const eventRepairer = await prisma.eventRepairer.findMany({
+    where: { eventId: id }
+  });
+
+  if (eventRepairer.length === 0) {
+    return [];
+  }
+
+  const userIds = eventRepairer.map((repairer) => repairer.userId);
+
+  // get additional informations like firstName, lastName, email
+  const repairersUsers: Partial<Record<string, User>> =
+    await userService.getUserMapFromIds(userIds);
+
+  return userIds
+    .map((userId) => {
+      const userData = repairersUsers[userId];
+      if (userData === undefined) {
+        return undefined;
+      }
+      return {
+        userId: userId,
+        firstName: userData?.firstName,
+        lastName: userData?.lastName,
+        email: userData?.emailAddress
+      };
+    })
+    .filter((user) => user !== undefined) as {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  }[];
 }
--- tests/pages/api/event/[id]/repairers/get.test.ts
+++ tests/pages/api/event/[id]/repairers/get.test.ts
@@ -5,7 +5,6 @@ import { beforeAll, describe, expect, it, vi } from "vitest";
 import { cleanup, seedTestData } from "@/../tests/utils";
 import endpoint from "@/pages/api/event/[id]/repairers";
 
-// Respect the Next.js config object if it's exported
 const handler: typeof endpoint & { config?: PageConfig } = endpoint;
 
 describe("GET /api/event/:id/repairers", () => {
@@ -14,9 +13,9 @@ describe("GET /api/event/:id/repairers", () => {
     await seedTestData();
     vi.mock("@clerk/nextjs/server");
     vi.mock("@clerk/nextjs");
-  }, 50000);
+  });
 
-  it("should be able to get event repairers and return json", async () => {
+  it("have matched event, repariers, should be able to return valid ones ", async () => {
     await testApiHandler({
       handler,
       params: { id: "acf5ed50-19a2-11ee-be56-0242ac120002" },
@@ -34,12 +33,6 @@ describe("GET /api/event/:id/repairers", () => {
             firstName: "test",
             lastName: "test",
             email: "test@gmail.com"
-          },
-          {
-            userId: "test_empty",
-            firstName: "",
-            lastName: "",
-            email: ""
           }
         ];
 
@@ -47,6 +40,26 @@ describe("GET /api/event/:id/repairers", () => {
 
         expect(res.status).toBe(200);
         expect(result).toMatchObject(expectedRepairers);
+      }
+    });
+  });
+
+  it("no matched event repairers, should be able to return an empty array ", async () => {
+    await testApiHandler({
+      handler,
+      params: { id: "not-exists" },
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+
+        const result = await res.json();
+
+        expect(res.status).toBe(200);
+        expect(result).toMatchObject([]);
       }
     });
   });
