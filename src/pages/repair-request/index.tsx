d897006b33d987427ab91a5ba9da07eb80e9a1e7
chloetychang <chloetychang@gmail.com>
2024-01-27 12:40:09 +0800

    refactored repair-request page, now we have to implement the reset function again

--- src/pages/repair-request/index.tsx
+++ src/pages/repair-request/index.tsx
@@ -1,58 +1,10 @@
 // Page for submitting a repair request
 import Image from "next/image";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
-
-import Button from "@/components/Button";
-import FieldImageUpload from "@/components/FormFields/field-image-upload";
-import SingleSelect from "@/components/FormFields/field-single-select";
-import FieldTextArea from "@/components/FormFields/field-text-area";
-import NavBar from "@/components/NavBar";
-import { TermsAndConditions } from "@/components/terms-and-conditions";
-import { Brand, useBrands } from "@/hooks/brands";
-import { EventOption, useEventOptions } from "@/hooks/events";
-import { ItemType, useItemTypes } from "@/hooks/item-types";
-import { useCreateRepairRequest } from "@/hooks/repair-request";
-import generateThumbnail from "@/lib/gen-thumbnail";
-import uploadImage from "@/lib/upload-image";
+import RepairRequestForm from "@/components/Forms/create-repair-request";
 import { NextPageWithLayout } from "@/pages/_app";
-import { createRepairRequestSchema } from "@/schema/repair-request";
-import { CreateRepairRequest } from "@/types";
+import NavBar from "@/components/NavBar";
 
 const RepairRequest: NextPageWithLayout = () => {
-  const { control, handleSubmit, setValue, reset } = useForm<FormValues>({
-    resolver: zodResolver(repairRequestFormSchema),
-    defaultValues: {
-      itemBrand: "",
-      itemType: "",
-      description: "",
-      images: [],
-      eventId: "",
-      comment: "",
-      tncAccepted: false
-    }
-  });
-
-  const { data: itemTypeList } = useItemTypes();
-  const { data: brandList } = useBrands();
-  const { data: eventOptions } = useEventOptions();
-  const { mutate: createRepairRequest } = useCreateRepairRequest();
-
-  const onSubmit: SubmitHandler<FormValues> = async (data) => {
-    const uploadPromises =
-      data.images?.map((image) => uploadImage(image)) ?? [];
-    const uploadThumbailPromise = data.images?.[0]
-      ? generateThumbnail(data.images[0]).then(uploadImage)
-      : undefined;
-    const keys = await Promise.all([uploadThumbailPromise, ...uploadPromises]);
-    const [thumbnailImage, ...images] = keys;
-    const updatedData = { ...data, thumbnailImage, images };
-    createRepairRequest(updatedData);
-    reset();
-    window.location.href = "/request-success";
-  };
-
   return (
     <div className="flex items-center justify-center p-4">
       <div className="flex w-screen flex-col justify-center gap-4 md:w-3/6 lg:w-4/12">
@@ -72,6 +24,7 @@ const RepairRequest: NextPageWithLayout = () => {
         {/* Heading of the Page */}
 
         <h1 className="flex justify-center text-xl font-bold">
+          {" "}
           Submit a Repair Request
         </h1>
         <RepairRequestForm />
