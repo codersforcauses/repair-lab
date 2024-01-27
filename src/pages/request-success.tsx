d32612e0f1d839f1466f437daf90b36584c6bf89
lumenesce <richellechen2000@gmail.com>
2024-01-27 12:16:57 +0800

    rearranged divs and stuff

--- src/pages/request-success.tsx
+++ src/pages/request-success.tsx
@@ -10,18 +10,18 @@ import type { NextPageWithLayout } from "./_app";
 // #TODO: Refactor this
 const Success: NextPageWithLayout = () => {
   return (
-    <div className="min-h-screen bg-lightAqua-100 flex justify-center">
-      <div className="relative flex flex-row h-screen items-center justify-center gap-x-12">
-        <div>
-          <Image src="/images/duck.png" alt="Duck" width={200} height={200} />
-        </div>
-
+    <div className="min-h-screen min-w-screen bg-lightAqua-100 flex justify-center">
+      <div className="relative flex h-screen items-center justify-center gap-x-12">
         <div className="w-1/2">
-          <div className="relative flex text-center mb-8 mt-6 text-3xl font-bold text-grey-900">
+          <div className="relative flex justify-center">
+            <Image src="/images/duck.png" alt="Duck" width={200} height={200} />
+          </div>
+
+          <div className="relative text-center mb-8 mt-6 text-3xl font-bold text-grey-900">
             Thank you!
           </div>
 
-          <div className="text-slate-500 text-xl mb-8 gap-y-4 leading-relaxed">
+          <div className="text-slate-500 text-xl text-center mb-8 gap-y-4 leading-relaxed">
             We have received your repair request, and you should receive a
             confirmation email shortly. We are currently experiencing high
             volumes of requests, and will aim to get back to you within 3-5
