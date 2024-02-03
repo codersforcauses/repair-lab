a84d1af93c8176e61c931c2b868b1386637e39de
lumenesce <richellechen2000@gmail.com>
2024-02-03 15:12:57 +0800

    added repair request ID to success page

--- src/pages/request-success/[id].tsx
+++ src/pages/request-success/[id].tsx
@@ -25,16 +25,20 @@ const Success: NextPageWithLayout = () => {
             />
           </div>
 
-          <div className="relative text-center mb-6 mt-8 text-3xl font-bold text-grey-900">
-            Thank you!!
-          </div>
+          <h1 className="relative text-center mb-6 mt-8 text-3xl font-bold text-grey-900">
+            Thank you!
+          </h1>
 
-          <div className="text-slate-500 text-xl text-center mb-8 gap-y-4 leading-relaxed">
+          <h2 className="relative text-center mb-3 mt-5 text-xl font-bold text-grey-500">
+            Repair request ID: {params.id}
+          </h2>
+
+          <p className="text-slate-500 text-xl text-center mb-8 gap-y-4 leading-relaxed">
             We have received your repair request, and you should receive a
             confirmation email shortly. We are currently experiencing high
             volumes of requests, and will aim to get back to you within 3-5
             business days. We appreciate your patience! In the meantime...
-          </div>
+          </p>
 
           <div className="relative flex text-center gap-x-8">
             <div className="w-1/2">
