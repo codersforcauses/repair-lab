775ecd4cdc5721d649a8d73a3a649eedc1301891
SetroZ <=>
2024-02-08 23:43:15 +0800

    modified clerk users and fixed mocks

--- tests/pages/api/event/aget-all.test.ts
+++ tests/pages/api/event/aget-all.test.ts
@@ -2,11 +2,12 @@ import type { PageConfig } from "next";
 import { testApiHandler } from "next-test-api-route-handler";
 import { beforeAll, describe, it, vi } from "vitest";
 
-// 
+//
 import endpoint from "@/pages/api/event";
 import { UserRole } from "@/types";
-
+import { clerkClient } from "@clerk/nextjs";
 import { cleanup, seedTestData } from "../../../utils";
+import userService from "@/services/user";
 
 // Respect the Next.js config object if it's exported
 const handler: typeof endpoint & { config?: PageConfig } = endpoint;
@@ -14,25 +15,21 @@ describe("GET /api/event", () => {
   beforeAll(async () => {
     await cleanup();
     await seedTestData();
+
     vi.mock("@clerk/nextjs/server", () => {
       return {
         getAuth: vi
           .fn()
+          .mockReturnValueOnce({ userId: "Test" })
           .mockReturnValueOnce({ userId: "REPAIRER" })
-          .mockReturnValueOnce({ userId: "ADMIN" })
       };
     });
-    vi.doMock("@/services/user", () => {
-      return {
-        getRole: vi
-          .fn()
-          .mockReturnValueOnce(UserRole.REPAIRER)
-          .mockReturnValueOnce(UserRole.ADMIN)
-      };
-    });
+    vi.mock("@clerk/nextjs");
+  
   }, 90000);
 
   it("should return 1 event for REPAIRER", async () => {
+    clerkClient.users.getUser("user_2")
     await testApiHandler({
       handler,
       test: async ({ fetch }) => {
@@ -42,6 +39,7 @@ describe("GET /api/event", () => {
             "Content-Type": "application/json"
           }
         });
+
         const result = await res.json();
       }
     });
