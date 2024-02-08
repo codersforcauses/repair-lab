0b6f3565b2da04b526ecda2ec1d17e518ad71b31
SetroZ <=>
2024-02-09 00:11:37 +0800

    added data for tests. Now working

--- tests/pages/api/event/aget-all.test.ts
+++ tests/pages/api/event/aget-all.test.ts
@@ -1,35 +1,48 @@
 import type { PageConfig } from "next";
 import { testApiHandler } from "next-test-api-route-handler";
-import { beforeAll, describe, it, vi } from "vitest";
+import { beforeAll, describe, expect, it, vi } from "vitest";
 
 //
 import endpoint from "@/pages/api/event";
-import { UserRole } from "@/types";
-import { clerkClient } from "@clerk/nextjs";
-import { cleanup, seedTestData } from "../../../utils";
-import userService from "@/services/user";
 
+import prisma from "../../../setup";
+import { cleanup, seedTestData } from "../../../utils";
 // Respect the Next.js config object if it's exported
 const handler: typeof endpoint & { config?: PageConfig } = endpoint;
 describe("GET /api/event", () => {
   beforeAll(async () => {
     await cleanup();
     await seedTestData();
+    await prisma.event.create({
+      data: {
+        createdBy: "mock user",
+        name: "Ipad",
+        location: "UWA",
+        eventType: "Laptop",
+        description: "Ipad repair",
+        disclaimer: "This is a disclaimer",
+        startDate: new Date("2023-11-16T01:27:08.417Z"),
+        endDate: new Date("2023-11-17T01:27:08.417Z"),
+        eventRepairer: {
+          create: {
+            userId: "REPAIRER"
+          }
+        }
+      }
+    });
 
     vi.mock("@clerk/nextjs/server", () => {
       return {
         getAuth: vi
           .fn()
-          .mockReturnValueOnce({ userId: "Test" })
           .mockReturnValueOnce({ userId: "REPAIRER" })
+          .mockReturnValueOnce({ userId: "Test" })
       };
     });
     vi.mock("@clerk/nextjs");
-  
   }, 90000);
 
   it("should return 1 event for REPAIRER", async () => {
-    clerkClient.users.getUser("user_2")
     await testApiHandler({
       handler,
       test: async ({ fetch }) => {
@@ -41,10 +54,12 @@ describe("GET /api/event", () => {
         });
 
         const result = await res.json();
+        console.log(result);
+        expect(result.items).toHaveLength(1);
       }
     });
   }, 90000);
-  it("should return 2 events for ADMIN", async () => {
+  it("should return 2 events for other users", async () => {
     await testApiHandler({
       handler,
       test: async ({ fetch }) => {
@@ -55,6 +70,8 @@ describe("GET /api/event", () => {
           }
         });
         const result = await res.json();
+        console.log(result);
+        expect(result.items).toHaveLength(2);
       }
     });
   });
