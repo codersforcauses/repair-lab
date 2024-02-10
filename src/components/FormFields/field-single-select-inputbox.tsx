2a4e9777801a80ac13f35b87b7eeca333896cc3f
HarryKomah <harrykomah2003@gmail.com>
2024-02-10 16:01:59 +0800

    adding input box in single select dropdown and adding plus button to push it into options

--- src/components/FormFields/field-single-select-inputbox.tsx
+++ src/components/FormFields/field-single-select-inputbox.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { useState } from "react";
+import { CiCirclePlus } from "react-icons/ci";
 import { Menu, Transition } from "@headlessui/react";
 import {
   FieldValues,
@@ -11,6 +12,7 @@ import { HiCheck, HiChevronDown } from "react-icons/hi";
 import Label from "@/components/FormFields/box-label";
 import Error from "@/components/FormFields/error-msg";
 import { Option } from "@/types";
+import { s } from "vitest/dist/reporters-5f784f42";
 
 export interface FormProps<T extends FieldValues = FieldValues>
   extends UseControllerProps<T> {
@@ -50,6 +52,38 @@ export default function FieldSingleSelect<T extends FieldValues = FieldValues>({
   const baseStyle = `flex ${height} ${width} justify-between overflow-hidden rounded-lg bg-white px-3 py-2.5 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset hover:shadow-grey-300`;
   const normalBorderStyle = `ring-grey-300`;
   const errorBorderStyle = `ring-red-500`;
+
+  function renderMenuItem(option: any) {
+    return (
+      <Menu.Item key={option.id}>
+        {({ active }) => (
+          <a
+            href="#"
+            onClick={() => {
+              field.onChange(option.id);
+              setDisplayText(option.text);
+            }}
+            className={classNames(
+              active ? "bg-lightAqua-100 text-grey-900" : "text-grey-900",
+              "block py-2 pl-2 pr-4 text-sm"
+            )}
+          >
+            {option.id === field.value ? (
+              <span className="relative left-0 flex">
+                <HiCheck
+                  className="h-5 w-5 text-darkAqua-600"
+                  aria-hidden="true"
+                />
+                <span className="pl-2 font-medium">{option.text}</span>
+              </span>
+            ) : (
+              <span className="pl-7">{option.text}</span>
+            )}
+          </a>
+        )}
+      </Menu.Item>
+    );
+  }
   return (
     <div className={`relative mb-2 inline-block ${width} text-left`}>
       <Menu>
@@ -89,43 +123,34 @@ export default function FieldSingleSelect<T extends FieldValues = FieldValues>({
         >
           <Menu.Items className="absolute left-0 z-10 mt-2 max-h-60 w-full min-w-min origin-top overflow-auto rounded-md bg-white shadow-lg ring-1 ring-grey-800 ring-opacity-10 focus:outline-none">
             <div className="py-1">
-              {options.map((option) => (
-                <Menu.Item key={option.id}>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      onClick={() => {
-                        field.onChange(option.id);
-                        setDisplayText(option.text);
-                      }}
-                      className={classNames(
-                        active
-                          ? "bg-lightAqua-100 text-grey-900"
-                          : "text-grey-900",
-                        "block py-2 pl-2 pr-4 text-sm"
-                      )}
-                    >
-                      {option.id === field.value ? (
-                        <span className="relative left-0 flex">
-                          <HiCheck
-                            className="h-5 w-5 text-darkAqua-600"
-                            aria-hidden="true"
-                          />
-                          {option.id === "input-box-add" ? (
-                            <input type={option.text} name={option.text} />
-                          ) : (
-                            <span className="pl-2 font-medium">
-                              {option.text}
-                            </span>
-                          )}
-                        </span>
-                      ) : (
-                        <span className="pl-7">{option.text}</span>
-                      )}
-                    </a>
-                  )}
-                </Menu.Item>
-              ))}
+              {options.map((option) => {
+                if (option.id === "input-box-add") {
+                  return (
+                    <Menu.Item disabled>
+                      <div className="flex flex-row">
+                        <input
+                          type="text"
+                          className="block ml-8 m-1 py-1 pl-2 text-sm w-5/6 overflow-hidden"
+                          onChange={(e) => setDisplayText(e.target.value)}
+                        />
+                        <button
+                          onClick={() => {
+                            const newItemType: Option = {
+                              id: `added-item-${options.length}`,
+                              text: displayText
+                            };
+                            options.push(newItemType);
+                          }}
+                        >
+                          <CiCirclePlus size={35} />
+                        </button>
+                      </div>
+                    </Menu.Item>
+                  );
+                } else {
+                  return renderMenuItem(option);
+                }
+              })}
             </div>
           </Menu.Items>
         </Transition>
