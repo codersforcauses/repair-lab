919e5a7653b5bf7aca7deb18d3e9b03f113f2328
tong <bluedragon00000@gmail.com>
2024-02-14 00:04:54 +0800

    move the sql into unit test to avoid data conflicts with other tests

--- tests/pages/api/event/[id]/repairers/get.test.ts
+++ tests/pages/api/event/[id]/repairers/get.test.ts
@@ -1,8 +1,11 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 import type { PageConfig } from "next";
 import { testApiHandler } from "next-test-api-route-handler";
 import { beforeAll, describe, expect, it, vi } from "vitest";
 
 import { cleanup, seedTestData } from "@/../tests/utils";
+import prisma from "@/lib/prisma";
 import endpoint from "@/pages/api/event/[id]/repairers";
 
 const handler: typeof endpoint & { config?: PageConfig } = endpoint;
@@ -11,8 +14,54 @@ describe("GET /api/event/:id/repairers", () => {
   beforeAll(async () => {
     await cleanup();
     await seedTestData();
-    vi.mock("@clerk/nextjs/server");
+
+    await prisma.event.create({
+      data: {
+        id: "acf5ed50-19a2-11ee-be56-0242ac120003",
+        createdBy: "Mock Repairer",
+        name: "Test Laptop Repair Event",
+        location: "Curtin University",
+        eventType: "Laptop",
+        description: "Test Laptop repair event.",
+        disclaimer: "This is a disclaimer",
+        startDate: new Date("2024-01-24T01:27:08.417Z"),
+        endDate: new Date("2024-01-24T01:27:08.417Z")
+      }
+    });
+
+    await prisma.eventRepairer.create({
+      data: {
+        id: "88676ba2-8d86-49b1-9969-ba3997917575",
+        userId: "Mock Repairer",
+        eventId: "acf5ed50-19a2-11ee-be56-0242ac120003"
+      }
+    });
+
+    vi.mock("@clerk/nextjs/server", async () => {
+      return {
+        getAuth: vi.fn().mockReturnValue({ userId: "Mock Repairer" })
+      };
+    });
+
     vi.mock("@clerk/nextjs");
+
+    vi.mocked(clerkClient.users.getUserList).mockReturnValue(
+      Promise.resolve([
+        {
+          id: "Mock Repairer",
+          firstName: "Mock",
+          lastName: "Repairer",
+          emailAddresses: [
+            {
+              emailAddress: "test@gmail.com"
+            }
+          ],
+          publicMetadata: {
+            role: "CLIENT"
+          }
+        }
+      ] as unknown as User[])
+    );
   });
 
   it("have matched event, repariers, should be able to return valid ones ", async () => {
@@ -29,12 +78,11 @@ describe("GET /api/event/:id/repairers", () => {
 
         const expectedRepairers = [
           {
-            userId: "mock user",
-            firstName: "test",
-            lastName: "test",
+            userId: "Mock Repairer",
+            firstName: "Mock",
+            lastName: "Repairer",
             email: "test@gmail.com"
-          },
-          { userId: "test_empty" }
+          }
         ];
 
         const result = await res.json();
--- tests/utils.ts
+++ tests/utils.ts
@@ -36,30 +36,17 @@ export const seedTestData = async () => {
   });
 
   await prisma.event.createMany({
-    data: [
-      {
-        id: "acf5ed50-19a2-11ee-be56-0242ac120002",
-        createdBy: "mock user",
-        name: "Laptop Repair Event",
-        location: "Curtin University",
-        eventType: "Laptop",
-        description: "Laptop repair event.",
-        disclaimer: "This is a disclaimer",
-        startDate: new Date("2023-11-16T01:27:08.417Z"),
-        endDate: new Date("2023-11-17T01:27:08.417Z")
-      },
-      {
-        id: "acf5ed50-19a2-11ee-be56-0242ac120003",
-        createdBy: "mock user",
-        name: "Test Laptop Repair Event",
-        location: "Curtin University",
-        eventType: "Laptop",
-        description: "Test Laptop repair event.",
-        disclaimer: "This is a disclaimer",
-        startDate: new Date("2024-01-24T01:27:08.417Z"),
-        endDate: new Date("2024-01-24T01:27:08.417Z")
-      }
-    ]
+    data: {
+      id: "acf5ed50-19a2-11ee-be56-0242ac120002",
+      createdBy: "mock user",
+      name: "Laptop Repair Event",
+      location: "Curtin University",
+      eventType: "Laptop",
+      description: "Laptop repair event.",
+      disclaimer: "This is a disclaimer",
+      startDate: new Date("2023-11-16T01:27:08.417Z"),
+      endDate: new Date("2023-11-17T01:27:08.417Z")
+    }
   });
 
   await prisma.repairRequest.create({
@@ -73,20 +60,5 @@ export const seedTestData = async () => {
       comment: "Some comment",
       thumbnailImage: ""
     }
-  });
-
-  await prisma.eventRepairer.createMany({
-    data: [
-      {
-        id: "88676ba2-8d86-49b1-9969-ba3997917575",
-        userId: "mock user",
-        eventId: "acf5ed50-19a2-11ee-be56-0242ac120003"
-      },
-      {
-        id: "88676ba2-8d86-49b1-9969-ba3997917577",
-        userId: "test_empty",
-        eventId: "acf5ed50-19a2-11ee-be56-0242ac120003"
-      }
-    ]
   });
 };
