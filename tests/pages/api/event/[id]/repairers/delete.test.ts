1fdf82c51f602889b631971ed65eb997d0e4d046
Stanley Natalius <22915411@student.uwa.edu.au>
2024-03-05 02:05:50 +0800

    improved on and added more tests for DELETE endpoint

--- tests/pages/api/event/[id]/repairers/delete.test.ts
+++ tests/pages/api/event/[id]/repairers/delete.test.ts
@@ -1,10 +1,10 @@
 import { PageConfig } from "next";
 import { testApiHandler } from "next-test-api-route-handler";
-import { clerkClient } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/server";
 import { beforeAll, describe, expect, it, vi } from "vitest";
 
 import endpoint from "@/pages/api/event/[id]/repairers";
+import userService from "@/services/user";
+import { User } from "@/types";
 
 import prisma from "../../../../../setup";
 import { cleanup, seedTestData } from "../../../../../utils";
@@ -31,12 +31,24 @@ describe("DELETE /api/event/[id]/repairers", () => {
       }
     });
 
-    await prisma.eventRepairer.create({
-      data: {
-        id: "88676ba2-8d86-49b1-9969-ba3997917575",
-        userId: "Mock Repairer",
-        eventId: "acf5ed50-19a2-11ee-be56-0242ac120004"
-      }
+    await prisma.eventRepairer.createMany({
+      data: [
+        {
+          id: "88676ba2-8d86-49b1-9969-ba3997917575",
+          userId: "Mock Repairer",
+          eventId: "acf5ed50-19a2-11ee-be56-0242ac120004"
+        },
+        {
+          id: "88676ba2-8d86-49b1-9969-ba3997917576",
+          userId: "Jim",
+          eventId: "acf5ed50-19a2-11ee-be56-0242ac120004"
+        },
+        {
+          id: "88676ba2-8d86-49b1-9969-ba3997917577",
+          userId: "John",
+          eventId: "acf5ed50-19a2-11ee-be56-0242ac120004"
+        }
+      ]
     });
 
     vi.mock("@clerk/nextjs/server", async () => {
@@ -46,29 +58,22 @@ describe("DELETE /api/event/[id]/repairers", () => {
     });
 
     vi.mock("@clerk/nextjs");
-
-    vi.mocked(clerkClient.users.getUser).mockImplementation(
-      async (userId: string) => {
-        return [
-          {
-            id: "Mock Repairer",
-            firstName: "Mock",
-            lastName: "Repairer",
-            emailAddresses: [
-              {
-                emailAddress: "test@gmail.com"
-              }
-            ],
-            publicMetadata: {
-              role: "CLIENT"
-            }
-          }
-        ].find((user) => user.id === userId) as unknown as User;
-      }
-    );
+    vi.mock("@/services/user");
   });
 
-  it.only("should be able to delete repairers from an event", async () => {
+  it("should be able to delete a single repairer from an event", async () => {
+    vi.mocked(userService.getUsers).mockReturnValue(
+      Promise.all([
+        {
+          id: "Mock Repairer",
+          firstName: "Mock",
+          lastName: "Repairer",
+          emailAddress: "mock@repairer.com",
+          role: "EVENT_MANAGER"
+        }
+      ] as User[])
+    );
+
     await testApiHandler({
       handler,
       params: {
@@ -88,6 +93,92 @@ describe("DELETE /api/event/[id]/repairers", () => {
         expect(res.status).toBe(200);
 
         // TODO - Add more assertions after figuring out how to mock getting Clerk Users
+      }
+    });
+  });
+
+  it("should be able to remove multiple repairers from an event", async () => {
+    vi.mocked(userService.getUsers).mockReturnValue(
+      Promise.all([
+        {
+          id: "Jim",
+          firstName: "Jim",
+          lastName: "Doe",
+          emailAddress: "jim@email.com",
+          role: "REPAIRER"
+        },
+        {
+          id: "John",
+          firstName: "John",
+          lastName: "Doe",
+          emailAddress: "john@email.com",
+          role: "REPAIRER"
+        }
+      ] as User[])
+    );
+
+    await testApiHandler({
+      handler,
+      params: {
+        id: "acf5ed50-19a2-11ee-be56-0242ac120004"
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            userId: ["Jim", "John"]
+          })
+        });
+
+        expect(res.status).toBe(200);
+      }
+    });
+  });
+
+  it("should return 404 if event not found", async () => {
+    await testApiHandler({
+      handler,
+      params: {
+        id: "non-existent-id"
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            userId: ["Jim", "John"]
+          })
+        });
+
+        expect(res.status).toBe(404);
+      }
+    });
+  });
+
+  it("should return 404 if repairer not found", async () => {
+    vi.mocked(userService.getUsers).mockReturnValue(Promise.all([] as User[]));
+    await testApiHandler({
+      handler,
+      params: {
+        id: "acf5ed50-19a2-11ee-be56-0242ac120004"
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            userId: ["NotExists"]
+          })
+        });
+
+        expect(res.status).toBe(404);
       }
     });
   });
